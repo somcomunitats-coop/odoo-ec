3626138817dbd69be49105f57604e730bfbca0f2
xavier.bonet <xavier.bonet@somenergia.coop>
2022-09-28 21:07:46 +0200

    bump to v12.0.0.0.7

--- ce/__manifest__.py
+++ ce/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "Comunitats Energètiques customizations",
-    'version': '12.0.0.0.6',
+    'version': '12.0.0.0.7',
     'depends': [
         'base_rest_base_structure',
         'cooperator_website',
