7ee980e6f5f2b664286c8ab77282636f9fad9ad5
Daniil Digtyar <daniildigtyar@gmail.com>
2022-12-29 10:25:10 +0100

    Initial ce_auth externalization

---
+++ ce_auth/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': "Energy Communities integration with authoidc",
+    'version': '14.0.0.0.0',
+    'depends': [
+        'auth_oauth',
+        'auth_oidc',
+    ],
+    'author': "Coopdevs Treball SCCL & Som Energia SCCL",
+    'website': 'https://somenergia.coop',
+    'category': "Cooperative management",
+    'description': """
+    Energy Communities integration with authoidc.
+    """,
+    "license": "AGPL-3",
+    'demo': [
+
+    ],
+    'data': [
+        'data/auth_oauth_provider_data.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False
+}
+
+
