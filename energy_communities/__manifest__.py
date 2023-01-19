55d4f57e3811c76171a57142c4d4f36d78d8c6a5
Daniil Digtyar Vasilieva <daniildigtyar@gmail.com>
2023-01-19 16:44:33 +0100

    Moved groups to security and updated names

--- energy_communities/__manifest__.py
+++ energy_communities/__manifest__.py
@@ -26,7 +26,6 @@
     'data': [
         'security/ir_rules_data.xml',
         'security/res_users_role_data.xml',
-        'data/res_groups_data.xml',
         'data/utm_data.xml',
         'data/crm_lead_tag.xml',
         'views/crm_lead_views.xml',
