a067b6a8f1e8b082c84ad359e0f2bac66abb6454
Daniil Digtyar Vasilieva <daniildigtyar@gmail.com>
2023-01-19 15:03:17 +0100

    Removed account_invoice.py

--- energy_communities/models/__init__.py
+++ energy_communities/models/__init__.py
@@ -2,7 +2,6 @@ from . import res_company
 from . import res_partner
 from . import subscription_request
 from . import res_users
-from . import account_invoice
 from . import product
 from . import utm_source
 from . import crm_lead
