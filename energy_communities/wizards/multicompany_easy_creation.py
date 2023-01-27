1f353b18713d92054a91bf3b0f59682479d4c174
Daniil Digtyar Vasilieva <daniildigtyar@gmail.com>
2023-01-27 14:49:48 +0100

    Added property_cooperator_account to wizzard

--- energy_communities/wizards/multicompany_easy_creation.py
+++ energy_communities/wizards/multicompany_easy_creation.py
@@ -5,3 +5,15 @@ class AccountMulticompanyEasyCreationWiz(models.TransientModel):
     _inherit = "account.multicompany.easy.creation.wiz"
 
     crm_lead_id = fields.Many2one('crm.lead', string="CRM Lead")
+    property_cooperator_account = fields.Many2one(
+        comodel_name="account.account",
+        string="Cooperator Account",
+        domain=[
+            ("internal_type", "=", "receivable"),
+            ("deprecated", "=", False),
+        ],
+        help="This account will be"
+             " the default one as the"
+             " receivable account for the"
+             " cooperators",
+    )
