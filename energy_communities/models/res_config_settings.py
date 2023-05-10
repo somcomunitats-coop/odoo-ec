f0022f4fe3dc0cf68285a09e201331d8cbf26c79
konykon <kon.tsagari@coopdevs.org>
2023-05-10 23:15:52 +0200

    Rename wordpress credentials variables

--- energy_communities/models/res_config_settings.py
+++ energy_communities/models/res_config_settings.py
@@ -6,19 +6,19 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     ''' WORDPRESS DB CREDENTIALS '''
-    wordpress_db_credentials_admin_username = fields.Char(
-        related='company_id.wordpress_db_credentials_admin_username',
-        string=_("Wordpress DB Admin Username"),
+    wordpress_db_username = fields.Char(
+        related='company_id.wordpress_db_username',
+        string=_("Wordpress DB Username"),
         readonly=False)
-    wordpress_db_credentials_admin_password = fields.Char(
-        related='company_id.wordpress_db_credentials_admin_password',
-        string=_("Wordpress DB Admin Password"),
+    wordpress_db_password = fields.Char(
+        related='company_id.wordpress_db_password',
+        string=_("Wordpress DB Password"),
         readonly=False)
 
 def get_wordpress_db_credentials(company):
     return {
-        "admin_data": {
-            "username": company.wordpress_db_credentials_admin_username,
-            "password": company.wordpress_db_credentials_admin_password,
+        "data": {
+            "username": company.wordpress_db_username,
+            "password": company.wordpress_db_password,
         }
     }
