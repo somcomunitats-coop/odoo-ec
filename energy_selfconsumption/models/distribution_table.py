194216d38f3f4a16bed99b8846c5d3fc4add4864
Daniil Digtyar Vasilieva <daniildigtyar@gmail.com>
2023-05-23 18:30:41 +0200

    [IMP] added type field

--- energy_selfconsumption/models/distribution_table.py
+++ energy_selfconsumption/models/distribution_table.py
@@ -5,11 +5,17 @@ STATE_VALUES = [
     ("active", _("Active")),
 ]
 
+TYPE_VALUES = [
+    ("fixed", _("Fixed")),
+    ("horary", _("Horary")),
+]
+
 class DistributionTable(models.Model):
     _name = 'energy_selfconsumption.distribution_table'
     _description = 'Distribution Table'
 
     name = fields.Char()
     selfconsumption_project_id = fields.Many2one('energy_selfconsumption.selfconsumption')
+    type = fields.Selection(TYPE_VALUES, default="fixed", required=True, string="Modality")
     state = fields.Selection(STATE_VALUES, default="draft", required=True)
     supply_point_assignation_ids = fields.One2many('energy_selfconsumption.supply_point_assignation', 'distribution_table_id')
