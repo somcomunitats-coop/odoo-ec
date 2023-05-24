4c946f12f815a44cebd4c0bfa0760c0c1f43a038
Daniil Digtyar Vasilieva <daniildigtyar@gmail.com>
2023-05-24 12:21:48 +0200

    [IMP] filter out supply already in the distribution table

--- energy_selfconsumption/models/supply_point_assignation.py
+++ energy_selfconsumption/models/supply_point_assignation.py
@@ -1,15 +1,23 @@
 from odoo import fields, models, api
 
-
 class SupplyPointAssignation(models.Model):
     _name = 'energy_selfconsumption.supply_point_assignation'
     _description = 'Supply Point Assignation'
 
+
     @api.depends('distribution_table_id')
     def _compute_supply_point_filtered_ids(self):
+        '''
+        List of supply point of partners subscribed to the project and not in the list of the distribution table to
+        prevent multiple assignations of same supply point.
+        Used to filter out in the view.
+        :return:
+        '''
         for record in self:
-            record.supply_point_filtered_ids = record.distribution_table_id.selfconsumption_project_id.inscription_ids.mapped('partner_id.supply_ids')
-
+            record.supply_point_filtered_ids = \
+                record.distribution_table_id.selfconsumption_project_id.inscription_ids.mapped('partner_id.supply_ids') \
+                    .filtered_domain([('id', 'not in', record.distribution_table_id.supply_point_assignation_ids.mapped(
+                    'supply_point_id.id'))])
 
     distribution_table_id = fields.Many2one('energy_selfconsumption.distribution_table', required=True)
     supply_point_id = fields.Many2one('energy_selfconsumption.supply_point', required=True)
