f232f1a5b29f8a65ca910ad7084e0dedabd75301
Dani Quilez <dani@daniquilez.com>
2023-07-03 16:31:19 +0200

    remove unnecessary vars on Authenticate init

--- energy_communities/pywordpress_client/resources/authenticate.py
+++ energy_communities/pywordpress_client/resources/authenticate.py
@@ -5,11 +5,10 @@ class Authenticate:
     _name = "authenticate"
     _url_path = "/jwt-auth/v1/token"
 
-    def __init__(self, baseurl, username, password, token=None, **kwargs):
+    def __init__(self, baseurl, username, password):
         self.baseurl = baseurl
         self.username = username
         self.password = password
-        self.token = token
 
     def authenticate(self):
         """
