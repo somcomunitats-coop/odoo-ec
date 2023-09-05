7f84af06c93d461618a6e3945d1d30cc64b09052
Daniil Digtyar Vasilieva <daniildigtyar@gmail.com>
2023-09-05 12:26:33 +0200

    [IMP] energy_selfconsumption: overwrite _load_records to set the xml_id from the order field

--- energy_selfconsumption/models/reseller.py
+++ energy_selfconsumption/models/reseller.py
@@ -5,7 +5,7 @@ class Reseller(models.Model):
     _name = "energy_selfconsumption.reseller"
     _description = "Energy Reseller"
 
-    order = fields.Char(string="Nº de orden")
+    order = fields.Char(string="Nº de orden", required=True)
     name = fields.Char(string="Nombre empresa")
     address = fields.Char(string="Dirección empresa")
     zip = fields.Char(string="C.P.")
@@ -18,3 +18,14 @@ class Reseller(models.Model):
     uninscription_date = fields.Date(string="Fecha baja")
     web = fields.Char(string="Página web")
     state = fields.Char(string="Estado")
+
+    def _load_records(self, data_list, update=False):
+        new_data_list = []
+        for data in data_list:
+            values = data["values"]
+            xml_id = data.get("xml_id")
+            noupdate = data.get("noupdate")
+            if values.get("order"):
+                xml_id = "energy_selfconsumption.reseller_%s" % (values.get("order"))
+            new_data_list.append(dict(xml_id=xml_id, values=values, noupdate=noupdate))
+        return super()._load_records(new_data_list, update)
