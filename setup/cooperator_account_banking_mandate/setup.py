3f8a1c35764f0c7a9aabd2fb28829345c98ae162
konykon <kon.tsagari@coopdevs.org>
2023-09-07 16:03:44 +0200

    Modify setup dependancies versioning

--- setup/cooperator_account_banking_mandate/setup.py
+++ setup/cooperator_account_banking_mandate/setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     setup_requires=['setuptools-odoo'],
     odoo_addon={
         "depends_override": {
-            "cooperator_account_payment": "odoo14-addon-cooperator-account-payment==14.0.1.1.0",
+            "cooperator_account_payment": "odoo14-addon-cooperator-account-payment~=14.0.1",
         },
     }
 )
--- setup/energy_project_energy_communities/setup.py
+++ setup/energy_project_energy_communities/setup.py
@@ -4,8 +4,8 @@ setuptools.setup(
     setup_requires=['setuptools-odoo'],
     odoo_addon={
         "depends_override": {
-            "energy_project": "odoo14-addon-energy-project==14.0.1.2.0",
-            "energy_communities": "odoo14-addon-energy-communities==14.0.2.0.0",
+            "energy_project": "odoo14-addon-energy-project~=14.0.1",
+            "energy_communities": "odoo14-addon-energy-communities~=14.0.1",
         },
     },
     
