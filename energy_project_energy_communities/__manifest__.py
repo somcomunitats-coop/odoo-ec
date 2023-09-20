3536744bad9969f05a8ddb5a304e9900a7f4b28e
Daniil Digtyar Vasilieva <daniildigtyar@gmail.com>
2023-09-20 10:21:31 +0200

    [REL] energy_project_energy_communities: 14.0.1.2.0

--- energy_project_energy_communities/__manifest__.py
+++ energy_project_energy_communities/__manifest__.py
@@ -9,7 +9,7 @@
     "author": "Coopdevs Treball SCCL & Som Energia SCCL",
     "website": "https://coopdevs.org",
     "category": "Customizations",
-    "version": "14.0.1.1.2",
+    "version": "14.0.1.2.0",
     "depends": [
         "energy_project",
         "energy_communities",
