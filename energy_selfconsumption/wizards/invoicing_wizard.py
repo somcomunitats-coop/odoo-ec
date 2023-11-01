8f95a196ef0d3371132fdedb72caba633d4d32af
Daniil Digtyar Vasilieva <daniildigtyar@gmail.com>
2023-11-01 13:12:34 +0100

    [IMP] energy_selfconsumption: set contract_ids selected

--- energy_selfconsumption/wizards/invoicing_wizard.py
+++ energy_selfconsumption/wizards/invoicing_wizard.py
@@ -5,6 +5,7 @@ class InvoicingWizard(models.TransientModel):
     _name = "energy_selfconsumption.invoicing.wizard"
 
     power = fields.Float(string="Total Energy Generated (kWh)")
+    contract_ids = fields.Many2many("contract.contract")
 
     def generate_invoices(self):
         return True
