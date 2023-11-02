e5f0df3a857b6845a18bd3dba34fa3d2ec5cd0e1
konykon <kon.tsagari@coopdevs.org>
2023-11-02 20:26:03 +0100

    Add partner bank account restriction

--- energy_communities/models/__init__.py
+++ energy_communities/models/__init__.py
@@ -5,6 +5,7 @@ from . import landing_page
 from . import res_company
 from . import res_config_settings
 from . import res_partner
+from . import res_partner_bank
 from . import res_users
 from . import res_users_role
 from . import subscription_request
