f6b5a61415d9c5f5fde96f94b89786a404ee7e0a
konykon <kon.tsagari@coopdevs.org>
2023-11-08 13:51:38 +0100

    Update energy_project version

--- energy_project/__manifest__.py
+++ energy_project/__manifest__.py
@@ -9,7 +9,7 @@
     "author": "Coopdevs Treball SCCL & Som Energia SCCL",
     "website": "https://coopdevs.org",
     "category": "Customizations",
-    "version": "14.0.2.0.0",
+    "version": "14.0.2.1.0",
     "depends": [
         "base",
         "mail",
