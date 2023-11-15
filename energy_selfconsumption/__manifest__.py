2319775c10627f2822543170f4744a3c358022a9
Emanuel Buzey <buzeyemanuel@gmail.com>
2023-11-15 13:50:20 +0100

    [IMP] energy_selfconsumption: use _get_name_invoice_report method to manage invoice templates
    In this commit we finish defining how to handle the different templates using the _get_name_invoice_report() method to tell the action the name of the template to use.

--- energy_selfconsumption/__manifest__.py
+++ energy_selfconsumption/__manifest__.py
@@ -43,7 +43,6 @@
         "wizards/define_invoicing_mode_wizard_view.xml",
         "wizards/invoicing_wizard_views.xml",
         "reports/selfconsumption_reports.xml",
-        "reports/energy_delivered_invoice_template.xml",
-        "reports/energy_delivered_invoice_report.xml",
+        "reports/energy_delivered_invoice_template.xml"
     ],
 }
