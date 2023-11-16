4c390cec796f0ab96e77f9ded112a13edb991225
Daniil Digtyar Vasilieva <daniildigtyar@gmail.com>
2023-11-16 12:28:48 +0100

    [IMP] energy_selfconsumption: added unit testing to energy delivered generation contracts

--- energy_selfconsumption/tests/test_contract_generation_wizard.py
+++ energy_selfconsumption/tests/test_contract_generation_wizard.py
@@ -77,6 +77,17 @@ class TestContractGenerationWizard(TransactionCase):
                 "invoicing_mode": "power_acquired",
             }
         )
+        self.define_invoicing_mode_energy_delivered_wizard = self.env[
+            "energy_selfconsumption.define_invoicing_mode.wizard"
+        ].create(
+            {
+                "selfconsumption_id": self.selfconsumption.id,
+                "price": 0.1,
+                "recurring_interval": 1,
+                "recurring_rule_type": "monthly",
+                "invoicing_mode": "energy_delivered",
+            }
+        )
         self.contract_generation_wizard = self.env[
             "energy_selfconsumption.contract_generation.wizard"
         ].create(
@@ -108,6 +119,37 @@ class TestContractGenerationWizard(TransactionCase):
         )
         expected_quantity = 100 * 1 * (days_timedelta.days + 1)
         related_contract[0].recurring_create_invoice()
+        invoice = related_contract._get_related_invoices()
+        self.assertEqual(invoice.invoice_line_ids[0].quantity, expected_quantity)
+        self.assertEqual(invoice.invoice_line_ids[0].price_unit, 0.1)
+
+    def test_energy_delivered_generation_contracts(self):
+        res = (
+            self.define_invoicing_mode_energy_delivered_wizard.save_data_to_selfconsumption()
+        )
+        self.assertEqual(
+            res,
+            {
+                "type": "ir.actions.act_window_close",
+            },
+        )
+
+        res = self.contract_generation_wizard.generate_contracts_button()
+        self.assertEqual(res, True)
+
+        related_contract = self.env["contract.contract"].search(
+            [("project_id", "=", self.selfconsumption.project_id.id)]
+        )
+
+        wizard_id = self.env["energy_selfconsumption.invoicing.wizard"].create(
+            {
+                "contract_ids": [(6, 0, related_contract.ids)],
+                "power": 200,
+            }
+        )
+
+        expected_quantity = 200 * 1
+        wizard_id.generate_invoices()
         invoice = related_contract._get_related_invoices()
         self.assertEqual(invoice.invoice_line_ids[0].quantity, expected_quantity)
         self.assertEqual(invoice.invoice_line_ids[0].price_unit, 0.1)
