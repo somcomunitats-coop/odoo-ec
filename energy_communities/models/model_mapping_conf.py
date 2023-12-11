8869ee567b87c86de66f2543dec13d4b23b27719
daniquilez <dani.quilez@gmail.com>
2023-12-11 19:45:57 +0100

    beta wizard mapping

--- energy_communities/models/model_mapping_conf.py
+++ energy_communities/models/model_mapping_conf.py
@@ -9,15 +9,15 @@ _MAP__LEAD_METADATA__COMPANY_CREATION_WIZARD = {
     "ce_state": "state_id",
     "email_from": "email",
     "contact_phone": "phone",
-    # "current_lang": "default_lang_id",
-    # "ce_services": "ce_tag_ids",
+    "current_lang": "default_lang_id",
+    "ce_services": "ce_tag_ids",
     "ce_number_of_members": "ce_number_of_members",
     "ce_status": "ce_member_status",
     "ce_why_become_cooperator": "landing_why_become_cooperator",
     "ce_become_cooperator_process": "landing_become_cooperator_process",
-    # "ce_primary_image_file": "landing_primary_image_file",
-    # "ce_secondary_image_file": "landing_secondary_image_file",
-    # "ce_logo_image_file": "landing_logo_file",
+    "ce_primary_image_file": "landing_primary_image_file",
+    "ce_secondary_image_file": "landing_secondary_image_file",
+    "ce_logo_image_file": "landing_logo_file",
     "ce_type": "landing_community_type",
     "ce_legal_form": "legal_form",
     "ce_creation_date": "foundation_date",
@@ -28,3 +28,11 @@ _MAP__LEAD_METADATA__COMPANY_CREATION_WIZARD = {
     "ce_facebook_url": "ce_facebook_url",
     "ce_telegram_url": "ce_telegram_url",
 }
+_LEAD_METADATA__DATE_FIELDS = ["ce_creation_date"]
+_LEAD_METADATA__ENERGY_TAGS_FIELDS = ["ce_services"]
+_LEAD_METADATA__LANG_FIELDS = ["current_lang"]
+_LEAD_METADATA__IMAGE_FIELDS = [
+    "ce_primary_image_file",
+    "ce_secondary_image_file",
+    "ce_logo_image_file",
+]
