24fe540170575176812044e5ef311428f48ecb68
Daniil Digtyar Vasilieva <daniildigtyar@gmail.com>
2024-01-03 11:25:24 +0100

    [IMP] energy_project: add view of service assignation

--- energy_project/models/service_assignation.py
+++ energy_project/models/service_assignation.py
@@ -1,9 +1,17 @@
-from odoo import api, fields, models
+from odoo import _, api, fields, models
 
 
 class ServiceAssignation(models.Model):
     _name = "energy_project.service_assignation"
     _description = "Energy Services Assignation"
+
+    _sql_constraints = {
+        (
+            "unique_service_id_provider_id_project_id",
+            "unique (service_id, provider_id, project_id)",
+            _("There's already a service with this provider assigned to this project."),
+        )
+    }
 
     service_id = fields.Many2one(
         "energy_project.service", string="Service", required=True
@@ -11,4 +19,4 @@ class ServiceAssignation(models.Model):
     provider_id = fields.Many2one(
         "energy_project.provider", string="Provider", required=True
     )
-    project_id = fields.Many2one("energy_project.project")
+    project_id = fields.Many2one("energy_project.project", required=True)
