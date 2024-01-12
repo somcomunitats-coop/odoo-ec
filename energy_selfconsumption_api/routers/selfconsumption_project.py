470ffed9843d3c59b66321ce9beb1af946ccbe28
Juan Pedro Sánchez <juanpe.sn@gmail.com>
2024-01-12 17:38:38 +0100

    ♻️  [REF] created more acurated schemas for projects responses
    added pagination and more metainformacion to responses

--- energy_selfconsumption_api/routers/selfconsumption_project.py
+++ energy_selfconsumption_api/routers/selfconsumption_project.py
@@ -12,20 +12,28 @@ from odoo.api import Environment
 from odoo.addons.fastapi.dependencies import odoo_env
 
 from ..domain import get_selfconsumption_projects
-from ..schemas import SelfConsumptionProjectInfo
+from ..schemas.responses import (
+    ProjectsInfoListResponse,
+    SingleProjectInfoResponse,
+)
+from ..utils import make_list_response, make_single_response
 
 router = APIRouter(tags=["energy_selfconsumption"])
 
 
-@router.get("/projects", response_model=List[SelfConsumptionProjectInfo])
+@router.get("/projects", response_model=ProjectsInfoListResponse)
 def selfconsumption_projects(
     env: Annotated[Environment, Depends(odoo_env)]
-) -> List[SelfConsumptionProjectInfo]:
-    return get_selfconsumption_projects(env, cau=None)
+) -> ProjectsInfoListResponse:
+    projects = get_selfconsumption_projects(env, cau=None)
+
+    return make_list_response(projects)
 
 
-@router.get("/projects/{cau}", response_model=List[SelfConsumptionProjectInfo])
+@router.get("/projects/{cau}", response_model=SingleProjectInfoResponse)
 def get_selfconsumption_project_by_cau(
     cau: str, env: Annotated[Environment, Depends(odoo_env)]
-) -> List[SelfConsumptionProjectInfo]:
-    return get_selfconsumption_projects(env, cau)
+) -> SingleProjectInfoResponse:
+    project = get_selfconsumption_projects(env, cau)
+
+    return make_single_response(project)
