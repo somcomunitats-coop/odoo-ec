b746e0a393b16460b13906ee7751e6e43bb7af6d
Emanuel Buzey <buzeyemanuel@gmail.com>
2024-01-17 17:15:06 +0100

    [REF] energy_selfconsumption: add 6 decimals to the field

--- energy_selfconsumption/models/supply_point.py
+++ energy_selfconsumption/models/supply_point.py
@@ -64,7 +64,7 @@ class SupplyPoint(models.Model):
     supplier_id = fields.Many2one("energy_project.supplier", string="Supplier")
     cadastral_reference = fields.Char(string="Cadastral reference")
     contracted_power = fields.Float(
-        string="Contracted power", digits=(10, 2), help="Value in kilowatts (kW)"
+        string="Contracted power", digits=(10, 6), help="Value in kilowatts (kW)"
     )
 
     @api.onchange("partner_id")
