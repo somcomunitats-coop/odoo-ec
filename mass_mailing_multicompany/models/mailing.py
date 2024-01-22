5270e78fe048e59a228c6da52d69c309de07f047
daniquilez <dani.quilez@gmail.com>
2024-01-22 15:53:41 +0100

    control multi-company selections on mailing.mailing

--- mass_mailing_multicompany/models/mailing.py
+++ mass_mailing_multicompany/models/mailing.py
@@ -1,11 +1,14 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class MassMailing(models.Model):
     _name = "mailing.mailing"
-    _inherit = "mailing.mailing"
-    # _inherit = ["multi.company.abstract", "mailing.mailing"]
+    _inherit = ["mailing.mailing", "user.currentcompany.mixin"]
 
     company_id = fields.Many2one(
         "res.company", default=lambda self: self.env.company, required=True
     )
+
+    @api.depends("company_id")
+    def _compute_user_current_company(self):
+        super()._compute_user_current_company()
