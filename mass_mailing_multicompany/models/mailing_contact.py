b4518ba392b333cbf7fb5e277e00045109451c2f
daniquilez <dani.quilez@gmail.com>
2024-01-22 17:53:43 +0100

    control partner selection on mailing contact respecting multicompany

--- mass_mailing_multicompany/models/mailing_contact.py
+++ mass_mailing_multicompany/models/mailing_contact.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class MassMailingContact(models.Model):
@@ -8,3 +8,13 @@ class MassMailingContact(models.Model):
     company_id = fields.Many2one(
         "res.company", default=lambda self: self.env.company, required=True
     )
+
+    @api.onchange("company_id")
+    def _on_change_company_id(self):
+        for record in self:
+            # TODO: For being sure the contact belongs only to a contact of this company we would have to use company_id instead company_ids
+            return {
+                "domain": {
+                    "partner_id": [("company_ids", "in", [record.company_id.id])]
+                },
+            }
