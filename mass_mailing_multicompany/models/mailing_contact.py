3ea2ef7699f119dc758a45cbdd0fbf0b3f393564
daniquilez <dani.quilez@gmail.com>
2024-01-24 17:14:25 +0100

    control partner_is setup on mail contact creation based on company_ids

--- mass_mailing_multicompany/models/mailing_contact.py
+++ mass_mailing_multicompany/models/mailing_contact.py
@@ -12,9 +12,37 @@ class MassMailingContact(models.Model):
     @api.onchange("company_id")
     def _on_change_company_id(self):
         for record in self:
-            # TODO: For being sure the contact belongs only to a contact of this company we would have to use company_id instead company_ids
             return {
                 "domain": {
                     "partner_id": [("company_ids", "in", [record.company_id.id])]
                 },
             }
+
+    def _set_partner(self):
+        self.ensure_one()
+        if not self.email:
+            return
+        m_partner = self.env["res.partner"]
+        # Look for a partner with that email
+        email = self.email.strip()
+        partner = m_partner.search(
+            [
+                ("email", "=ilike", email),
+                ("company_ids", "in", [self.env.user.get_current_company_id()]),
+            ],
+            limit=1,
+        )
+        if partner:
+            # Partner found
+            self.partner_id = partner
+        else:
+            lts = self.subscription_list_ids.mapped("list_id") | self.list_ids
+            if lts.filtered("partner_mandatory"):
+                # Create partner
+                partner_vals = self._prepare_partner()
+                self.partner_id = m_partner.sudo().create(partner_vals)
+
+    def _prepare_partner(self):
+        partner_vals = super()._prepare_partner()
+        partner_vals["company_ids"] = [(4, self.env.user.get_current_company_id())]
+        return partner_vals
