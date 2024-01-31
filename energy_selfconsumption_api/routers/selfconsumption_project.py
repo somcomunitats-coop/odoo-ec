b680c0febf480daa8c0a0888bdefad4bf1d80ae1
Juan Pedro Sánchez <juanpe.sn@gmail.com>
2024-01-31 15:16:01 +0100

    [IMP] & [REF] add selfconsumption_project_members endpoing and clear code for paging, dependecies and responses
    * add selfconsumption_project_members to retrive the members that belogns to an energy selfconsumtion project
    * simplify Paging and EnergySelfconsumptionService dependencies
    * use PaginationLinks schema for paging instead of dict
    * add response class to generic response functionstype <description>

--- energy_selfconsumption_api/routers/selfconsumption_project.py
+++ energy_selfconsumption_api/routers/selfconsumption_project.py
@@ -1,15 +1,13 @@
 import logging
 
-from fastapi import APIRouter, Depends, Request
-from typing_extensions import Annotated
+from fastapi import APIRouter, Request
 
-from ..dependencies import energy_selfconsumption_service, paging
-from ..schemas import PaginationLimits
+from ..dependencies import EnergySelfconsumptionDep, PagingDep
 from ..schemas.responses import (
+    ProjectMembersResponse,
     ProjectsInfoListResponse,
     SingleProjectInfoResponse,
 )
-from ..services import EnergySelfconsumptionService
 from ..utils import collection_response, make_single_response
 
 logger = logging.getLogger("__name__")
@@ -24,17 +22,16 @@ router = APIRouter(tags=["energy_selfconsumption"])
 )
 def selfconsumption_projects(
     request: Request,
-    paging: Annotated[PaginationLimits, Depends(paging)],
-    energy_selfconsumption_service: Annotated[
-        EnergySelfconsumptionService, Depends(energy_selfconsumption_service)
-    ],
+    paging: PagingDep,
+    energy_selfconsumption_service: EnergySelfconsumptionDep,
 ) -> ProjectsInfoListResponse:
     total_projects = energy_selfconsumption_service.total_selfconsumption_projects
     projects = energy_selfconsumption_service.selfconsumption_projects(
         limit=paging.limit, offset=paging.offset
     )
-
-    return collection_response(request, projects, total_projects, paging)
+    return collection_response(
+        request, ProjectsInfoListResponse, projects, total_projects, paging
+    )
 
 
 @router.get(
@@ -45,11 +42,30 @@ def selfconsumption_projects(
 def get_selfconsumption_project_by_code(
     project_code: str,
     request: Request,
-    energy_selfconsumption_service: Annotated[
-        EnergySelfconsumptionService, Depends(energy_selfconsumption_service)
-    ],
+    energy_selfconsumption_service: EnergySelfconsumptionDep,
 ) -> SingleProjectInfoResponse:
     projects = energy_selfconsumption_service.selfconsumption_projects(project_code)
     project = projects[0] if projects and len(projects) > 0 else None
+    return make_single_response(request, SingleProjectInfoResponse, project)
 
-    return make_single_response(project, request)
+
+@router.get(
+    "/projects/{project_code}/members",
+    response_model=ProjectMembersResponse,
+    name="selfconsumption_project_members",
+)
+def selfconsumption_project_members(
+    project_code: str,
+    request: Request,
+    paging: PagingDep,
+    energy_selfconsumption_service: EnergySelfconsumptionDep,
+) -> ProjectMembersResponse:
+    total_projectmembers = energy_selfconsumption_service.total_project_members(
+        project_code
+    )
+    members = energy_selfconsumption_service.selfconsumption_project_members(
+        project_code, limit=paging.limit, offset=paging.offset
+    )
+    return collection_response(
+        request, ProjectMembersResponse, members, total_projectmembers, paging
+    )
--- energy_selfconsumption_api/services/energy_selfconsumption.py
+++ energy_selfconsumption_api/services/energy_selfconsumption.py
@@ -4,12 +4,21 @@ from odoo.api import Environment
 
 from odoo.addons.base.models.res_users import Users
 
-from ..schemas import SelfConsumptionProjectInfo
+from ..schemas import SelfConsumptionProjectInfo, SelfConsumptionProjectMember
 
 
 class EnergySelfconsumptionService:
     def __init__(self, env: Environment, user: Users) -> None:
-        self.model = env["energy_selfconsumption.selfconsumption"].with_user(user)
+        self.Selfconsumption = env["energy_selfconsumption.selfconsumption"].with_user(
+            user
+        )
+        self.SupplyPointAssignation = env[
+            "energy_selfconsumption.supply_point_assignation"
+        ].with_user(user)
+
+    @property
+    def total_selfconsumption_projects(self) -> int:
+        return self.Selfconsumption.search_count([])
 
     def selfconsumption_projects(
         self,
@@ -26,9 +35,46 @@ class EnergySelfconsumptionService:
                 energy_community_name=project.company_id.name,
                 power=project.power,
             )
-            for project in self.model.search(search_domain, limit=limit, offset=offset)
+            for project in self.Selfconsumption.search(
+                search_domain, limit=limit, offset=offset
+            )
         ]
 
-    @property
-    def total_selfconsumption_projects(self) -> int:
-        return self.model.search_count([])
+    def _get_selfconsumption_project(self, project_code: str):
+        project = self.Selfconsumption.search([("code", "=", project_code)])
+        return project and project[0] or None
+
+    def total_project_members(self, project_code: str) -> int:
+        project = self._get_selfconsumption_project(project_code)
+        search_domain = [
+            ("selfconsumption_project_id", "=", project.id),
+        ]
+        return self.SupplyPointAssignation.search_count(search_domain)
+
+    def selfconsumption_project_members(
+        self,
+        project_code: str,
+        limit: int = None,
+        offset: int = None,
+    ) -> List[SelfConsumptionProjectMember]:
+        project = self._get_selfconsumption_project(project_code)
+
+        search_domain = [
+            ("selfconsumption_project_id", "=", project.id),
+        ]
+        return [
+            SelfConsumptionProjectMember(
+                supply_point_code=assignation.supply_point_id.code,
+                supply_point_address=assignation.supply_point_id.street,
+                supply_point_postalcode=assignation.supply_point_id.zip,
+                supply_point_town=assignation.supply_point_id.city,
+                supply_point_state=assignation.supply_point_id.state_id.name,
+                distribution_coefficient=assignation.coefficient,
+                owner_name=assignation.owner_id.firstname,
+                owner_surnames=assignation.owner_id.lastname,
+                owner_vat=assignation.owner_id.vat or "",
+            )
+            for assignation in self.SupplyPointAssignation.search(
+                search_domain, limit=limit, offset=offset
+            )
+        ]
