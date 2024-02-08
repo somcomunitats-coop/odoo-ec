144667279059f4ac730707e10ac4a8c56d0ad9cd
Emanuel Buzey <buzeyemanuel@gmail.com>
2024-02-08 10:59:45 +0100

    [IMP] energy_selfconsumption: add contract mandate module

--- setup/energy_selfconsumption/setup.py
+++ setup/energy_selfconsumption/setup.py
@@ -7,6 +7,7 @@ setuptools.setup(
             "energy_project": "odoo14-addon-energy-project==14.0.2.1.1",
             "web_m2x_options": "odoo14-addon-web-m2x-options==14.0.1.1.1",
             "contract_queue_job": "odoo14-addon-contract-queue-job==14.0.1.0.1.dev3",
+            "contract_mandate": "odoo14-addon-contract-mandate=14.0.1.0.1",
         }
     }
 )
