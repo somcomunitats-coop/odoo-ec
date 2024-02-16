c97311b3a7363a757a343ab647a98a4f50eadc09
Daniil Digtyar Vasilieva <daniildigtyar@gmail.com>
2024-02-16 13:28:53 +0100

    [IMP] energy_project: added new field user_id to indicate user api of the service

--- energy_project/models/provider.py
+++ energy_project/models/provider.py
@@ -9,3 +9,4 @@ class Provider(models.Model):
     service_available_ids = fields.One2many(
         "energy_project.service_available", "provider_id"
     )
+    user_id = fields.Many2one("res.users", required=True)
