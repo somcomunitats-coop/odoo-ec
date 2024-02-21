d127f479c8efae1864252cb7747f9aaae70c6597
Juan Pedro Sánchez <juanpe.sn@gmail.com>
2024-02-21 17:06:09 +0100

    WIP:🚧 playing with authorization
    Co-authored-by: Dani Quilez <dani.quilez@somenergia.coop>

--- energy_selfconsumption_api/__manifest__.py
+++ energy_selfconsumption_api/__manifest__.py
@@ -15,12 +15,12 @@
         "energy_selfconsumption",
     ],
     "data": [
-        "security/ir_rule_data.xml",
-        "security/res_users_role_data.xml",
+        "data/service_data.xml",
         "data/fastapi_endpoint_data.xml",
         "data/provider_data.xml",
-        "data/service_data.xml",
         "data/service_available_data.xml",
+        "security/res_users_role_data.xml",
+        "security/ir_rule_data.xml",
     ],
     "demo": ["demo/energy_selfconsumption_app_demo.xml"],
 }
