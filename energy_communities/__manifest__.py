227727581d5888fc8db4daab54ea71da20e96bc1
daniquilez <dani.quilez@gmail.com>
2024-02-26 11:18:03 +0100

    Delete restriction on mass_mailing menu item. Allowed now for users.

--- energy_communities/__manifest__.py
+++ energy_communities/__manifest__.py
@@ -73,7 +73,6 @@
         "views/subscription_request_view.xml",
         "views/res_users_view.xml",
         "views/res_users_role_view.xml",
-        "views/mailing_mailing_views_menus.xml",
         "views/menus.xml",
         "views/website_formfields_template.xml",
         "views/website_subscription_template.xml",
