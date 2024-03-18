b65ff95a292ffbcfa48d1e192a0d0254ac1007fa
Juan Pedro Sánchez <juanpe.sn@gmail.com>
2024-03-18 10:17:45 +0100

    [REL] 🐛 fix energy-community-mass-mailing dependency versions
    also remove energy-community-mass-mailing from energy-communities
    
    Co-authored-by: Dani Quilez <dani.quilez@somenergia.coop>
    Co-authored-by: Daniil Digtyar <daniil.digtyar@somit.coop>

--- setup/energy_communities_mass_mailing/setup.py
+++ setup/energy_communities_mass_mailing/setup.py
@@ -2,5 +2,11 @@ import setuptools
 
 setuptools.setup(
     setup_requires=['setuptools-odoo'],
-    odoo_addon=True,
+    odoo_addon={
+       "depends_override": {
+           "energy_communities":  "odoo-adddon-energy-communities==14.0.8.0.1",
+           "mass_mailing_parter":  "odoo-adddon-mass-mailing-parner==14.0.1.1.0",
+           "mass_mailing_list_dynamic":  "odoo-adddon-mass-mailing-list-dynamic==14.0.1.0.1.dev4",
+       }
+    },
 )
