dda72b3830f4503ee65ff222e1f02d26992bd05b
Juan Pedro Sánchez <juanpe.sn@gmail.com>
2024-03-18 10:21:44 +0100

    [REL] 🏗️ update version energy_communities_mass_mailing to 14.0.0.1.1
    Co-authored-by: Dani Quilez <dani.quilez@somenergia.coop>
    Co-authored-by: Daniil Digtyar <daniil.digtyar@somit.coop>

--- energy_communities_mass_mailing/__manifest__.py
+++ energy_communities_mass_mailing/__manifest__.py
@@ -9,7 +9,7 @@
     "author": "Som Comunitats",
     "website": "https://coopdevs.org",
     "category": "Marketing",
-    "version": "14.0.0.1.0",
+    "version": "14.0.0.1.1",
     "license": "AGPL-3",
     # any module necessary for this one to work correctly
     "depends": [
