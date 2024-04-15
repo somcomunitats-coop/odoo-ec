273a661bea199af83c3cb628bfa9ea3609c33fd5
Daniil Digtyar Vasilieva <daniildigtyar@gmail.com>
2024-04-15 23:23:32 +0200

    [REV] "[IMP] energy_selfconsumption: set default order for assignations"
    This reverts commit 841653ad0ff6f2e84b3aed85b816702051ecaea8.

--- energy_selfconsumption/models/supply_point_assignation.py
+++ energy_selfconsumption/models/supply_point_assignation.py
@@ -5,7 +5,6 @@ from odoo.exceptions import ValidationError
 class SupplyPointAssignation(models.Model):
     _name = "energy_selfconsumption.supply_point_assignation"
     _description = "Supply Point Assignation"
-    _order = ["hour", "code"]
 
     @api.depends("distribution_table_id", "hour")
     def _compute_supply_point_filtered_ids(self):
