56ec88e9c31bd0a454cc14d070c179c610268475
Juan Pedro Sánchez <juanpe.sn@gmail.com>
2024-04-23 11:26:42 +0200

    [RELEASE] 🏗️ update versions of modules that depend of energy_communites
    bump energy_communities_api to 14.0.1.0.3

--- setup/energy_communities_api/setup.py
+++ setup/energy_communities_api/setup.py
@@ -4,12 +4,12 @@ setuptools.setup(
     setup_requires=['setuptools-odoo'],
     odoo_addon={
         "depends_override": {
-            "fastapi": "odoo14-addon-fastapi==14.0.0.0.3",
-            "energy_communities": "odoo14-addon-energy-communities==14.0.8.2.1"
+            "fastapi": "odoo14-addon-fastapi>=14.0.0.0.3",
+            "energy_communities": "odoo14-addon-energy-communities>=14.0.8.2.1"
         },
         "external_dependencies_override": {
             "python": {
-                "httpx": "httpx==0.27.0"
+                "httpx": "httpx>=0.27.0"
             },
         },
     },
