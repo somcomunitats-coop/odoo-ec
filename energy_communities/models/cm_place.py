f355aec20fd665ceef701cf302dd3defc3e4e478
konykon <kon.tsagari@coopdevs.org>
2024-04-24 00:44:13 +0200

    Change field Char to Many2one

--- energy_communities/models/cm_place.py
+++ energy_communities/models/cm_place.py
@@ -5,8 +5,11 @@ from odoo.tools.translate import _
 class CmPlace(models.Model):
     _inherit = "cm.place"
 
-    landing_reference = fields.Char(
-        string=_("Landing reference"), compute="_compute_landing_reference", store=False
+    landing_reference = fields.Many2one(
+        "landing.page",
+        string=_("Landing reference"),
+        compute="_compute_landing_reference",
+        store=False,
     )
 
     def _compute_landing_reference(self):
