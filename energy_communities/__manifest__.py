cb033cdcf837bcd7c722d8e458a9290f4cab9326
Juan Pedro Sánchez <juanpe.sn@gmail.com>
2024-05-07 10:54:55 +0200

    [RELEASE]🏗️ bump versions of energy_communities, energy_communites_cooperator, energy_communities_crm
    bump energy_communities to v14.0.9.0.0
    bump energy_communities_cooperator to v14.0.1.0.0 new release! ✨
    bump energy_communities_crm to v14.0.1.0.0 new release! ✨

--- energy_communities/__manifest__.py
+++ energy_communities/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Energy Community",
-    "version": "14.0.8.3.0",
+    "version": "14.0.9.0.0",
     "depends": [
         "account",
         "cooperator_account_banking_mandate",
--- energy_communities_crm/__manifest__.py
+++ energy_communities_crm/__manifest__.py
@@ -12,7 +12,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     "category": "Sales/CRM",
-    "version": "14.0.0.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     # any module necessary for this one to work correctly
     "depends": [
