0b776b93599a634b9e3e751812a188e9bfc40276
Juan Pedro Sánchez <juanpe.sn@gmail.com>
2024-05-21 15:38:13 +0200

    🏗️ [RELEASE]: Bump module versions of energy_communities, energy_communities_crm,energy_communities_crm,energy_selfconsumption
    bump energy_communities to v14.0.9.1.0
    bump energy_communities_cooperator to v14.0.1.0.1
    bump energy_communities_crm to v14.0.1.1.0
    bump energy_selfconsumption to v14.0.4.0.3
    
    Co-authored-by: Dani Quilez <dani.quilez@somenergia.coop>

--- energy_communities_cooperator/__manifest__.py
+++ energy_communities_cooperator/__manifest__.py
@@ -12,7 +12,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     "category": "Cooperative management",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "license": "AGPL-3",
     # any module necessary for this one to work correctly
     "depends": ["base", "cooperator", "l10n_es_cooperator", "energy_communities"],
