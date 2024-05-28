0e7fab21ca7637cd656fbee2345dc51a9182e101
Juan Pedro Sánchez <juanpe.sn@gmail.com>
2024-05-28 13:32:54 +0200

    [IMP]✨ energy_project: new functions to MonitoringService
    add generated_energy_by_member, energy_selfconsumed_ratio_by_member, energy_surplus_ratio_by_member

--- energy_project/services/monitoring_service.py
+++ energy_project/services/monitoring_service.py
@@ -1,6 +1,54 @@
+from functools import lru_cache, reduce
+
 from ..backends.base import Backend
+
+# Functions to manage attributes access and operations
+_get_energy_production = lambda point: float(point.get("energy_production", 0))
+_get_energy_selfconsumption = lambda point: float(point.get("selfconsumption", 0))
+_get_energy_exported = lambda point: float(point.get("energy_exported", 0))
+_selfconsumption_ratio_pair = lambda accumulator, point: (
+    accumulator[0] + _get_energy_selfconsumption(point),
+    accumulator[1] + _get_energy_production(point),
+)
+_selfconsumption_surplus_ratio_pair = lambda accumulator, point: (
+    accumulator[0] + _get_energy_exported(point),
+    accumulator[1] + _get_energy_production(point),
+)
 
 
 class MonitoringService:
     def __init__(self, backend: Backend):
         self.backend = backend
+
+    def generated_energy_by_member(self, system_id, member_id, date_from, date_to):
+        daily_metrics = self._get_daily_metrics_by_member(
+            system_id, member_id, date_from, date_to
+        )
+        energy = sum(map(_get_energy_production, daily_metrics))
+        return energy
+
+    def energy_selfconsumed_ratio_by_member(
+        self, system_id, member_id, date_from, date_to
+    ):
+        daily_metrics = self._get_daily_metrics_by_member(
+            system_id, member_id, date_from, date_to
+        )
+        selfconsumed_energy, generated_energy = reduce(
+            _selfconsumption_ratio_pair, daily_metrics, (0, 0)
+        )
+        return round(selfconsumed_energy / generated_energy, 4)
+
+    def energy_surplus_ratio_by_member(self, system_id, member_id, date_from, date_to):
+        daily_metrics = self._get_daily_metrics_by_member(
+            system_id, member_id, date_from, date_to
+        )
+        selfconsumed_surplus_energy, generated_energy = reduce(
+            _selfconsumption_surplus_ratio_pair, daily_metrics, (0, 0)
+        )
+        return round(selfconsumed_surplus_energy / generated_energy, 4)
+
+    @lru_cache
+    def _get_daily_metrics_by_member(self, system_id, member_id, from_date, to_date):
+        return self.backend.project_daily_metrics_by_member(
+            system_id, member_id, from_date, to_date
+        )
--- energy_project/tests/test_monitoring_service.py
+++ energy_project/tests/test_monitoring_service.py
@@ -1,8 +1,10 @@
+from datetime import date
+
 from odoo.tests.common import TransactionCase
 
 from ..backends import ArkenovaBackend
 from ..services import MonitoringService
-from .backends_data import arkenova_data
+from .backends_data import arkenova_data, member_code, project_code
 
 
 class TestMonitoringService(TransactionCase):
@@ -19,3 +21,83 @@ class TestMonitoringService(TransactionCase):
 
         # then we have an instance of that service
         self.assertIsInstance(monitoring_service, MonitoringService)
+
+    def test__generated_energy_by_member(self):
+        # given two dates
+        date_from = date(2024, 4, 1)
+        date_to = date(2024, 4, 30)
+        # a valid system id
+        system_id = project_code
+        # a member id
+        member_id = member_code
+        # and a monitoring service
+        monitoring_service = MonitoringService(backend=self.backend)
+
+        # when we ask for the generated_energy for that user
+        generated_energy = monitoring_service.generated_energy_by_member(
+            system_id, member_id, date_from, date_to
+        )
+
+        # then we obtain the value of the generated energy between that dates
+        self.assertEqual(generated_energy, 1425.198)
+
+    def test__energy_selfconsumed_ratio_by_member(self):
+        # given two dates
+        date_from = date(2024, 4, 1)
+        date_to = date(2024, 4, 30)
+        # a valid system id
+        system_id = project_code
+        # a member id
+        member_id = member_code
+        # and a monitoring service
+        monitoring_service = MonitoringService(backend=self.backend)
+
+        # when we ask for percentage of energy self-consumed
+        selfconsumed_ratio = monitoring_service.energy_selfconsumed_ratio_by_member(
+            system_id, member_id, date_from, date_to
+        )
+
+        # then we obtain the value of the generated energy between that dates
+        self.assertEqual(selfconsumed_ratio, 0.0668)
+
+    def test__energy_selfconsumption_surplus_by_member(self):
+        # given two dates
+        date_from = date(2024, 4, 1)
+        date_to = date(2024, 4, 30)
+        # a valid system id
+        system_id = project_code
+        # a member id
+        member_id = member_code
+        # and a monitoring service
+        monitoring_service = MonitoringService(backend=self.backend)
+
+        # when we ask for percentage of energy exported to the energy grid
+        surplus_ratio = monitoring_service.energy_surplus_ratio_by_member(
+            system_id, member_id, date_from, date_to
+        )
+
+        # then we obtain the percentage of the energy exported
+        self.assertEqual(surplus_ratio, 0.9332)
+
+    def test__relation_energy_selfconsumed_surplus_by_member(self):
+        # given two dates
+        date_from = date(2024, 4, 1)
+        date_to = date(2024, 4, 30)
+        # a valid system id
+        system_id = project_code
+        # a member id
+        member_id = member_code
+        # and a monitoring service
+        monitoring_service = MonitoringService(backend=self.backend)
+
+        # when we ask for the percentage of energy exported to the energy grid
+        surplus_ratio = monitoring_service.energy_surplus_ratio_by_member(
+            system_id, member_id, date_from, date_to
+        )
+        # and we ask for the percentage of energy selfconsumed
+        selfconsumed_ratio = monitoring_service.energy_selfconsumed_ratio_by_member(
+            system_id, member_id, date_from, date_to
+        )
+
+        # then the sum must be 1
+        self.assertEqual(surplus_ratio + selfconsumed_ratio, 1.0)
