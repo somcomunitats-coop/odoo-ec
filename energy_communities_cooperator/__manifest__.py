e60d4992948bf3e7179ba1f2267c5a7d0081a5c1
daniquilez <dani.quilez@gmail.com>
2024-06-03 15:47:56 +0200

    bump modules version for pypi deployment

--- energy_communities_cooperator/__manifest__.py
+++ energy_communities_cooperator/__manifest__.py
@@ -12,7 +12,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     "category": "Cooperative management",
-    "version": "14.0.1.0.1",
+    "version": "14.0.1.0.2",
     "license": "AGPL-3",
     # any module necessary for this one to work correctly
     "depends": ["base", "cooperator", "l10n_es_cooperator", "energy_communities"],
--- energy_selfconsumption/__manifest__.py
+++ energy_selfconsumption/__manifest__.py
@@ -9,7 +9,7 @@
     "author": "Coopdevs Treball SCCL & Som Energia SCCL",
     "website": "https://coopdevs.org",
     "category": "Customizations",
-    "version": "14.0.4.0.3",
+    "version": "14.0.4.0.4",
     "depends": [
         "base",
         "mail",
