2ad565d44bb1b051b80dbd3b7e5a9bc15229bb8d
daniquilez <dani.quilez@gmail.com>
2024-06-05 16:17:52 +0200

    bump new cooperator version on setup

--- setup/energy_communities/setup.py
+++ setup/energy_communities/setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
             "account_lock_date_update": "odoo14-addon-account-lock-date-update==14.0.2.0.1.dev10",
             "account_reconciliation_widget": "odoo14-addon-account-reconciliation-widget==14.0.2.0.2",
             "community_maps": "odoo14-addon-community-maps==14.0.0.2.6",
-            "cooperator": "odoo14-addon-cooperator==14.0.2.0.5",
+            "cooperator": "odoo14-addon-cooperator==14.0.2.1.1",
             "cooperator_account_payment": "odoo14-addon-cooperator-account-payment==14.0.1.1.0",
             "cooperator_account_banking_mandate": "odoo14-addon-cooperator-account-banking-mandate==14.0.1.1.1",
             "energy_selfconsumption": "odoo14-addon-energy-selfconsumption==14.0.4.0.4",
