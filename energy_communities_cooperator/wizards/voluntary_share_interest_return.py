1593a40c4d3b925a7427af5070ea4713dbc40e79
daniquilez <dani.quilez@gmail.com>
2024-06-25 16:00:51 +0200

    base invoice collection set generation

--- energy_communities_cooperator/wizards/voluntary_share_interest_return.py
+++ energy_communities_cooperator/wizards/voluntary_share_interest_return.py
@@ -1,11 +1,13 @@
+from datetime import datetime, timedelta
+
 from odoo import api, fields, models
 from odoo.exceptions import ValidationError
 from odoo.tools.translate import _
 
 _DEFAULT_ANUAL_BULK_INTEREST_RATE = 1
 _DEFAULT_IRPF_CAPITAL_TAX_XMLID = "account_tax_template_p_irpf19"
-_DEFAULT_CREDIT_ACCOUNT_MOVE_XMLID = "account_common_6624"
-_DEFAULT_RETURN_ACCOUNT_MOVE_LINE_XMLID = "account_common_4100"
+_DEFAULT_CREDIT_ACCOUNT_MOVE_XMLID = "account_common_4100"
+_DEFAULT_RETURN_ACCOUNT_MOVE_LINE_XMLID = "account_common_6624"
 
 # NOTE: we'll have to build this xml_ids dynamically prepending on each "l10n_es.{company_id}_"
 
@@ -17,14 +19,56 @@ class VoluntaryShareInterestReturn(models.TransientModel):
     company_id = fields.Many2one("res.company")
     interest = fields.Float(string="Interest")
     credit_account_move_id = fields.Many2one(
-        "account.account", string="Account move credit account"
+        "account.account",
+        string="Account move credit account",
     )
     return_line_account_id = fields.Many2one(
-        "account.account", string="Return line account"
+        "account.account",
+        string="Return line account",
     )
     journal_id = fields.Many2one("account.journal", string="Journal")
+    tax_id = fields.Many2one("account.tax", string="Tax")
+    payment_mode_id = fields.Many2one("account.payment.mode", string="Payment mode")
     start_date_period = fields.Date(string="Period start date")
     end_date_period = fields.Date(string="Period end date")
+
+    def default_get(self, fields):
+        res = super().default_get(fields)
+        res.update(
+            {
+                "interest": _DEFAULT_ANUAL_BULK_INTEREST_RATE,
+                "credit_account_move_id": self._default_obj_id_by_ref(
+                    _DEFAULT_CREDIT_ACCOUNT_MOVE_XMLID
+                ),
+                "return_line_account_id": self._default_obj_id_by_ref(
+                    _DEFAULT_RETURN_ACCOUNT_MOVE_LINE_XMLID
+                ),
+                "tax_id": self._default_obj_id_by_ref(_DEFAULT_IRPF_CAPITAL_TAX_XMLID),
+                "journal_id": self.env.company.voluntary_share_journal_account.id
+                or False,
+                "start_date_period": self._default_period_date("start"),
+                "end_date_period": self._default_period_date("end"),
+            }
+        )
+        return res
+
+    def _default_period_date(self, mode):
+        # return (datetime.now() - timedelta(days=365)).replace(day=1,month=1)
+        if mode == "start":
+            return datetime.now() - timedelta(days=365)
+        if mode == "end":
+            return datetime.now()
+
+    def _build_l10n_multicompany_xml_id(self, base_ref):
+        return "l10n_es.{company_id}_{ref}".format(
+            company_id=self.env.company.id, ref=base_ref
+        )
+
+    def _default_obj_id_by_ref(self, base_ref):
+        return self.env.ref(
+            self._build_l10n_multicompany_xml_id(base_ref),
+            raise_if_not_found=True,
+        ).id
 
     def execute_return(self):
         self._consistency_validation()
@@ -43,7 +87,14 @@ class VoluntaryShareInterestReturn(models.TransientModel):
             },
         }
 
-    def _generate_related_invoices(self):
+    # UNUSED!!
+    # TODO: Verify this is the criteria to include an invoice.
+    # Should it be effective based on payment date?
+    def _in_impacted_period(self, account_move):
+        return account_move.payment_date >= self.start_date_period
+
+    def _get_voluntary_shares_invoice_line(self):
+        voluntary_shares = {}
         voluntary_share_product = self.company_id.voluntary_share_id
         memberships = self.env["cooperative.membership"].search(
             [("company_id", "=", self.company_id.id)]
@@ -54,14 +105,97 @@ class VoluntaryShareInterestReturn(models.TransientModel):
                 == voluntary_share_product.id
             )
             if membership_voluntary_shares:
-                invoice = self._create_interest_return_invoice()
                 for membership_voluntary_share in membership_voluntary_shares:
-                    pass
+                    related_invoice_line = (
+                        self._find_related_invoice_line_from_share_line(
+                            membership_voluntary_share
+                        )
+                    )
+                    # if self._in_impacted_period(related_invoice_line.move_id):
+                    if membership.id in voluntary_shares.keys():
+                        voluntary_shares[membership.id].append(related_invoice_line)
+                    else:
+                        voluntary_shares[membership.id] = [related_invoice_line]
+        return voluntary_shares
 
-    def _create_interest_return_invoice(self):
-        pass
+    def _generate_related_invoices(self):
+        voluntary_shares = self._get_voluntary_shares_invoice_line()
+        if voluntary_shares:
+            for membership_id in voluntary_shares.keys():
+                invoice = self._create_interest_return_invoice(
+                    membership_id, voluntary_shares[membership_id]
+                )
+
+    def _create_interest_return_invoice(self, membership_id, voluntary_shares_inv_line):
+        inv_creation_dict = self._prepare_inv_create_dict(
+            membership_id, voluntary_shares_inv_line
+        )
+        invoice = self.env["account.move"].create(inv_creation_dict)
+
+    def _prepare_inv_create_dict(self, membership_id, voluntary_shares_inv_line):
+        membership = self.env["cooperative.membership"].browse(membership_id)
+        create_dict = {
+            "partner_id": membership.partner_id.id,
+            "invoice_date": self.end_date_period,
+            "date": self.end_date_period,
+            "journal_id": self.journal_id.id,
+            "payment_mode_id": self.payment_mode_id.id,
+            "company_id": self.company_id.id,
+            "move_type": "in_invoice",
+            "partner_bank_id": self._get_invoice_partner_bank(
+                voluntary_shares_inv_line[0]
+            ),  # TODO: Find a proper way of getting partner bank account
+            "invoice_line_ids": [],
+        }
+        for voluntary_share_inv_line in voluntary_shares_inv_line:
+            create_dict["invoice_line_ids"].append(
+                (0, 0, self._prepare_inv_line_create_dict(voluntary_share_inv_line))
+            )
+        return create_dict
+
+    def _get_invoice_partner_bank(self, origin_inv_line):
+        # TODO: This method must take into consideration that the bank account has a valid mandate
+        origin_inv = origin_inv_line.move_id
+        if origin_inv_line.move_id.partner_bank_id:
+            return origin_inv.partner_bank_id.id
+        else:
+            rel_bank_acc = self.env["res.partner.bank"].search(
+                [("partner_id", "=", origin_inv.partner_id.id)]
+            )
+            if rel_bank_acc:
+                return rel_bank_acc[0].id
+        return False
+
+    def _prepare_inv_line_create_dict(self, origin_inv_line):
+        return {
+            "name": "Interest return #{inv_id}-{inv_line_id}".format(
+                inv_id=origin_inv_line.move_id.id, inv_line_id=origin_inv_line.id
+            ),
+            "account_id": self.return_line_account_id.id,
+            "quantity": self._get_voluntary_share_days_num(origin_inv_line),
+            "price_unit": self._get_voluntary_share_price_unit(origin_inv_line),
+            "journal_id": self.journal_id.id,
+            "company_id": self.company_id.id,
+            "tax_ids": [(4, self.tax_id.id)],
+        }
+
+    # TODO: verify this formula
+    def _get_voluntary_share_days_num(self, inv_line):
+        payment_date = inv_line.move_id.payment_date
+        if payment_date >= self.start_date_period:
+            return (self.end_date_period - payment_date).days
+        else:
+            return (self.end_date_period - self.start_date_period).days
+
+    def _get_voluntary_share_price_unit(self, inv_line):
+        return inv_line.price_subtotal * self.interest / 36500
 
     def _consistency_validation(self):
+        if self.env.company.id != self.company_id.id:
+            raise ValidationError(
+                "The company on your context must be the same as the one you're executing the action for"
+            )
+
         voluntary_share_product = self.company_id.voluntary_share_id
         # check if all shares comes from an invoice
         memberships = self.env["cooperative.membership"].search(
@@ -97,6 +231,19 @@ class VoluntaryShareInterestReturn(models.TransientModel):
                             )
                         )
                     )
+                if (
+                    related_invoice_line.move_id.payment_date
+                    != membership_voluntary_share.effective_date
+                ):
+                    raise ValidationError(
+                        _(
+                            "ERROR on Membership {membership_id}. Inconstency between share {share_id} effective date and related invoice payment_date".format(
+                                membership_id=membership.id,
+                                share_id=membership_voluntary_share.id,
+                            )
+                        )
+                    )
+
             if total_in_membership > 0:
                 related_membership_invoices = self.env["account.move"].search(
                     [
