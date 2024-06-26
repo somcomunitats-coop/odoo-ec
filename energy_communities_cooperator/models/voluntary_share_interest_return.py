c58ade039609111152e189ba4976611373b507a9
daniquilez <dani.quilez@gmail.com>
2024-06-26 15:31:57 +0200

    voluntary_share_interest_return base model

---
+++ energy_communities_cooperator/models/voluntary_share_interest_return.py
@@ -0,0 +1,16 @@
+from odoo import api, fields, models
+from odoo.exceptions import ValidationError
+from odoo.tools.translate import _
+
+
+class VoluntaryShareInterestReturn(models.Model):
+    _name = "voluntary.share.interest.return"
+    _inherit = ["mail.thread"]
+
+    name = fields.Char(string="Name")
+    start_date_period = fields.Date(string="Period start date")
+    end_date_period = fields.Date(string="Period end date")
+    company_id = fields.Many2one("res.company", string="Company")
+    account_move_ids = fields.One2many(
+        "account.move", "voluntary_share_interest_return_id", string="Moves"
+    )
