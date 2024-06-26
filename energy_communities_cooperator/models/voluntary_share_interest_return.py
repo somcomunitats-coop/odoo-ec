6f999938b88cbd5fc34494c42777a1a46b622459
daniquilez <dani.quilez@gmail.com>
2024-06-26 18:19:00 +0200

    interest return base views

--- energy_communities_cooperator/models/voluntary_share_interest_return.py
+++ energy_communities_cooperator/models/voluntary_share_interest_return.py
@@ -10,7 +10,9 @@ class VoluntaryShareInterestReturn(models.Model):
     name = fields.Char(string="Name")
     start_date_period = fields.Date(string="Period start date")
     end_date_period = fields.Date(string="Period end date")
-    company_id = fields.Many2one("res.company", string="Company")
+    company_id = fields.Many2one(
+        "res.company", string="Company", default=lambda self: self.env.company
+    )
     account_move_ids = fields.One2many(
         "account.move", "voluntary_share_interest_return_id", string="Moves"
     )
