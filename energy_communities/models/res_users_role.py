921405241f23f0d80dfc0ae2e6f8d2a2110a8e7d
konykon <kon.tsagari@coopdevs.org>
2024-06-28 16:00:44 +0200

    Add available_role_ids on res_users_role

--- energy_communities/models/res_users_role.py
+++ energy_communities/models/res_users_role.py
@@ -1,14 +1,26 @@
-from odoo import _, fields, models
+from odoo import fields, models
 
 
 class ResUsersRole(models.Model):
     _inherit = "res.users.role"
 
     code = fields.Char(string="Code")
-    priority = fields.Integer(string=_("Priority"))
+    priority = fields.Integer(string="Priority")
+    available_role_ids = fields.One2many(
+        "res.users.role", "available_role_id", string="Available roles"
+    )
+    available_role_id = fields.Many2one("res.users.role", string="Available role")
 
 
 class ResUsersRoleLine(models.Model):
     _inherit = "res.users.role.line"
+
+    _sql_constraints = [
+        (
+            "user_role_uniq",
+            "unique (user_id,role_id,company_id)",
+            "Roles can be assigned to a user only once at a time",
+        )
+    ]
 
     code = fields.Char(related="role_id.code")
