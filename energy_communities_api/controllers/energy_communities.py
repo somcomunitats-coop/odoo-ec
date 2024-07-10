3477dc31ac295e4781834403e958b6207268903a
daniquilez <dani.quilez@gmail.com>
2024-07-10 14:36:55 +0200

    start jwt auth structure

--- energy_communities_api/controllers/energy_communities.py
+++ energy_communities_api/controllers/energy_communities.py
@@ -4,5 +4,5 @@ from odoo.addons.base_rest.controllers import main
 class MemberProfileController(main.RestController):
     _root_path = "/api/energy-communities/"
     _collection_name = "energy_communities_member.api.services"
-    _default_auth = "api_key"
-    # _default_auth = "kc_jwt_auth"
+    # _default_auth = "api_key"
+    _default_auth = "jwt_energy_communities_auth"
