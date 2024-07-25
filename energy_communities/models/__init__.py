75524f5eaa913c855a8b30150ba62bfb980ad350
konykon <kon.tsagari@coopdevs.org>
2024-07-25 14:16:01 +0200

    Add model IrHttp and overwrite method session_info

--- energy_communities/models/__init__.py
+++ energy_communities/models/__init__.py
@@ -4,6 +4,7 @@ from . import cm_filter
 from . import cm_map
 from . import cm_place
 from . import external_id_mixin
+from . import ir_http
 from . import auth_oauth_provider
 from . import landing_page
 from . import res_company
