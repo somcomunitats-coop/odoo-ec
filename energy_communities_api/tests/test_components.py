b81629cb147d6e43d7ae19713b7e41939f636f7b
daniquilez <dani.quilez@gmail.com>
2024-08-06 13:54:05 +0200

    test get all result when no pagination

--- energy_communities_api/tests/test_components.py
+++ energy_communities_api/tests/test_components.py
@@ -4,7 +4,7 @@ from odoo.addons.component.core import WorkContext
 from odoo.addons.component.tests.common import TransactionComponentCase
 
 from ..components.partner_api_info import PartnerApiInfo
-from ..schemas import CommunityInfo, MemberInfo
+from ..schemas import CommunityInfo, MemberInfo, PaginationLimits
 
 
 class TestMemberApiInfo(TransactionComponentCase):
@@ -66,4 +66,35 @@ class TestMemberApiInfo(TransactionComponentCase):
                 CommunityInfo.from_orm(community)
                 for community in fake_energy_communities()
             ],
+        )
+
+    def test__get_member_communities_paging_params(self):  # , patcher):
+        def fake_energy_communities():
+            return [self.env["res.company"].browse(id_) for id_ in range(1, 3)]
+
+        # patcher.return_value = fake_energy_communities()
+
+        # given a energy community member
+        member = self.env.ref("cooperator.res_partner_cooperator_1_demo")
+        # given a paging object for 1 element and an api info component
+        page = 1
+        page_size = 1
+        paging = PaginationLimits(
+            limit=page_size, offset=(page - 1) * page_size, page=page
+        )
+        work = WorkContext(
+            "res.partner",
+            collection=self.backend,
+            schema_class=CommunityInfo,
+            paging=paging,
+        )
+        api_info_component = work.component(usage="api.info")
+
+        # when we ask for the energy_communities that this member belongs
+        member_communities = api_info_component.get_member_communities(member)
+
+        # then we have 1 communities
+        self.assertEqual(
+            len(member_communities),
+            1,
         )
