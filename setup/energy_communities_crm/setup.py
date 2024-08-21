760dcec2f7fc8cc0cd6201b7a8729d22822e0001
Juan Pedro Sánchez <juanpe.sn@gmail.com>
2024-08-21 15:45:36 +0200

    [REL] 🏗️  bump versions of energy_communities, energy_communities_api, energy_communities_cooperator, energy_communities_crm, energy_project, energy_selfconsumption
    * bump energy_communities to 14.0.9.4.0
    
    * bump energy_communities_api to 14.0.2.0.0
    
    * bump energy_communities_cooperator to  14.0.1.2.1
    
    * bump energy_communities_crm to 14.0.1.1.4
    
    * bump energy_project to 14.0.3.4.3
    
    * bump energy_selfconsumption to 14.0.5.2.0

--- setup/energy_communities_crm/setup.py
+++ setup/energy_communities_crm/setup.py
@@ -7,6 +7,7 @@ setuptools.setup(
             "crm_metadata": "odoo14-addon-crm-metadata==14.0.1.0.0",
             "crm_metadata_rest_api": "odoo14-addon-crm-metadata-rest-api==14.0.1.0.2",
             "crm_rest_api": "odoo14-addon-crm-rest-api==14.0.1.0.2",
+            "energy_communities": "odoo14-addon-energy-communities>=14.0.9.0.0",
         },
     }
 )
