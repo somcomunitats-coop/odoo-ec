c98aa7b20d31830e597271a871c5da326fe1c851
daniquilez <dani.quilez@gmail.com>
2024-09-24 12:43:44 +0200

    [IMP] ✨ energy_action xml_id field

--- energy_communities/models/energy_action.py
+++ energy_communities/models/energy_action.py
@@ -5,3 +5,9 @@ class EnergyAction(models.Model):
     _name = "energy.action"
 
     name = fields.Char(string="Name")
+    xml_id = fields.Char(compute="_compute_xml_id", string="External ID")
+
+    def _compute_xml_id(self):
+        res = self.get_external_id()
+        for record in self:
+            record.xml_id = res.get(record.id)
