2972bc9e63f064b2ff97e4e90dfc7091cb8686f8
Juan Pedro Sánchez <juanpe.sn@gmail.com>
2024-09-30 15:41:59 +0200

    [REL] 🏗️  bump versions of energy_communites, energy_communities_cooperator, energy_communties_crm, energy_selfconsumption
    * bump energy_communities to 14.0.9.5.0
    * bump energy_communities_cooperator to 14.0.1.2.3
    * bump energy_communities_crm to 14.0.1.2.0
    * bumpo energy_selfconsumption to 14.0.5.2.3

--- energy_selfconsumption/__manifest__.py
+++ energy_selfconsumption/__manifest__.py
@@ -9,7 +9,7 @@
     "author": "Coopdevs Treball SCCL & Som Energia SCCL",
     "website": "https://coopdevs.org",
     "category": "Customizations",
-    "version": "14.0.5.2.2",
+    "version": "14.0.5.2.3",
     "depends": [
         "base",
         "web",
