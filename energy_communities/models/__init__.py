0b99ea76c1c174598065581ee88b474fb86a01ef
daniquilez <dani.quilez@gmail.com>
2024-10-01 17:15:46 +0200

    [WIP] 🚧 Community energy action implementation

--- energy_communities/models/__init__.py
+++ energy_communities/models/__init__.py
@@ -4,6 +4,7 @@ from . import cm_filter
 from . import cm_map
 from . import cm_place
 from . import energy_action
+from . import community_energy_action
 from . import external_id_mixin
 from . import auth_oauth_provider
 from . import landing_page
---
+++ energy_communities/models/community_energy_action.py
@@ -0,0 +1,20 @@
+from odoo import fields, models
+
+
+class CommunityEnergyAction(models.Model):
+    _name = "community.energy.action"
+
+    energy_action_id = fields.Many2one(
+        "energy.action",
+        string="Energy Action",
+    )
+    company_id = fields.Many2one(
+        "res.company",
+        string="Company",
+    )
+    public_status = fields.Selection(
+        [("published", "Published"), ("hidden", "Hidden")],
+        string="Public status",
+        default="published",
+        required=True,
+    )
