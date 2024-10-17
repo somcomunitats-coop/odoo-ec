68f35968d60fa952dfcdd6a86c1dd09af590731d
daniquilez <dani.quilez@gmail.com>
2024-10-17 12:52:18 +0200

    [FIX] 🐛 Load demo data for energy_communities module
    Co-authored-by: Alvaro Garcia <alvaro.garcia@somit.coop>

--- energy_project/__manifest__.py
+++ energy_project/__manifest__.py
@@ -27,10 +27,11 @@
         "views/service_available_views.xml",
         "views/service_contract_views.xml",
         "views/res_config_settings_views.xml",
+        "demo/provider_demo.xml",
     ],
     "demo": [
-        "demo/service_demo.xml",
+        # "demo/service_demo.xml",
         "demo/provider_demo.xml",
-        "demo/service_available_demo.xml",
+        # "demo/service_available_demo.xml",
     ],
 }
--- setup/energy_communities_cooperator/setup.py
+++ setup/energy_communities_cooperator/setup.py
@@ -1,16 +1,15 @@
 import setuptools
 
 setuptools.setup(
-    setup_requires=['setuptools-odoo'],
+    setup_requires=["setuptools-odoo"],
     odoo_addon={
         "depends_override": {
             "account_multicompany_easy_creation": "odoo14-addon-account-multicompany-easy-creation>=14.0.1.0.1.dev2",
             "cooperator": "odoo14-addon-cooperator==14.0.2.1.1",
             "cooperator_account_payment": "odoo14-addon-cooperator-account-payment==14.0.1.1.0",
             "cooperator_account_banking_mandate": "odoo14-addon-cooperator-account-banking-mandate==14.0.1.1.2",
-            "l10n_es_cooperator": "odoo14-addon-l10n-es-cooperator==14.0.0.1.1",
+            "l10n_es_cooperator": "odoo-addon-l10n-es-cooperator==16.0.1.0.0.4",
             "energy_communities": "odoo14-addon-energy-communities>=14.0.9.0.0",
         },
     },
 )
-
