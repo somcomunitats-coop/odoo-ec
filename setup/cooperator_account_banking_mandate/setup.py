4bccda0bf70a060d8d676c136e925e004505cb77
daniquilez <dani.quilez@gmail.com>
2024-10-29 10:18:25 +0100

    [WIP] 🚧 modules setup configuration on v15

--- setup/cooperator_account_banking_mandate/setup.py
+++ setup/cooperator_account_banking_mandate/setup.py
@@ -1,12 +1,12 @@
 import setuptools
 
 setuptools.setup(
-    setup_requires=['setuptools-odoo'],
+    setup_requires=["setuptools-odoo"],
     odoo_addon={
         "depends_override": {
-            "cooperator": "odoo14-addon-cooperator>=14.0.2.1.1",
-            "account_banking_mandate": "odoo14-addon-account-banking-mandate==14.0.1.2.0",
-            "account_banking_sepa_direct_debit": "odoo14-addon-account-banking-sepa-direct-debit==14.0.1.3.3",
+            "cooperator": "odoo-addon-cooperator>=15.0.0.0.1",
+            "account_banking_mandate": "odoo-addon-account-banking-mandate==15.0.0.0.1",
+            "account_banking_sepa_direct_debit": "odoo-addon-account-banking-sepa-direct-debit==15.0.2.3.1",
         },
-    }
+    },
 )
--- setup/cooperator_account_payment/setup.py
+++ setup/cooperator_account_payment/setup.py
@@ -1,11 +1,11 @@
 import setuptools
 
 setuptools.setup(
-    setup_requires=['setuptools-odoo'],
+    setup_requires=["setuptools-odoo"],
     odoo_addon={
         "depends_override": {
-            "cooperator": "odoo14-addon-cooperator>=14.0.2.1.1",
-            "account_payment_partner": "odoo14-addon-account-payment-partner==14.0.1.7.0",
+            "cooperator": "odoo-addon-cooperator>=15.0.0.0.1",
+            "account_payment_partner": "odoo-addon-account-payment-partner==15.0.1.3.2",
         }
-    }
+    },
 )
