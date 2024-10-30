4d1a2b4daad2a7d0db65b4e46c2d3ed6372cf579
daniquilez <dani.quilez@gmail.com>
2024-10-30 13:29:39 +0100

    adjust wrong dependency on setup

--- setup/cooperator_account_banking_mandate/setup.py
+++ setup/cooperator_account_banking_mandate/setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     odoo_addon={
         "depends_override": {
             "cooperator": "odoo-addon-cooperator>=15.0.0.0.1",
-            "account_banking_mandate": "odoo-addon-account-banking-mandate==15.0.0.0.1",
+            "account_banking_mandate": "odoo-addon-account-banking-mandate==15.0.2.1.4",
             "account_banking_sepa_direct_debit": "odoo-addon-account-banking-sepa-direct-debit==15.0.2.3.1",
         },
     },
