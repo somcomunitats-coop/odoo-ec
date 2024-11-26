0274ff6b7d650c5293aef96260368ef4d27bf079
daniquilez <dani.quilez@gmail.com>
2024-11-26 15:31:33 +0100

    [REL] 🏗️  Increase energy_communities_mass_mailing and energy_selfconsumption modules version for deployment

--- energy_selfconsumption/__manifest__.py
+++ energy_selfconsumption/__manifest__.py
@@ -9,7 +9,7 @@
     "author": "Coopdevs Treball SCCL & Som Energia SCCL",
     "website": "https://coopdevs.org",
     "category": "Customizations",
-    "version": "14.0.5.2.5",
+    "version": "14.0.5.2.6",
     "depends": [
         "base",
         "web",
