5a0e0e4dc1d944e8dab44ce6e90ec3d966fe899a
daniquilez <dani.quilez@gmail.com>
2024-12-09 10:49:10 +0100

    [REL] 🏗️ bump cooperator and  selffconsumption module versions for peployment

--- energy_selfconsumption/__manifest__.py
+++ energy_selfconsumption/__manifest__.py
@@ -9,7 +9,7 @@
     "author": "Coopdevs Treball SCCL & Som Energia SCCL",
     "website": "https://coopdevs.org",
     "category": "Customizations",
-    "version": "16.0.0.1.0",
+    "version": "16.0.0.1.1",
     "license": "AGPL-3",
     "depends": [
         "base",
