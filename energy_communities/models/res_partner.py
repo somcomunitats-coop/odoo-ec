3f5f7c38388981ed2869e457e150ed486be18fa4
daniquilez <dani.quilez@gmail.com>
2025-02-11 10:57:30 +0100

    [FIX] 🐛 Allow energy modules installation

--- energy_communities/models/res_partner.py
+++ energy_communities/models/res_partner.py
@@ -59,16 +59,22 @@ class ResPartner(models.Model):
 
     def compute_company_hierarchy_level(self):
         for record in self:
-            if self.related_company_id:
-                record.company_hierarchy_level = self.related_company_id.hierarchy_level
+            try:
+                related_company_id = record.related_company_id
+            except:
+                related_company_id = False
+            if related_company_id:
+                record.company_hierarchy_level = (
+                    record.related_company_id.hierarchy_level
+                )
 
     def compute_related_company_id(self):
         for record in self:
             related_company_id = self.env["res.company"].search(
-                [("partner_id", "=", record.id)]
+                [("partner_id", "=", record.id)], limit=1
             )
             if related_company_id:
-                record.related_company_id = related_company_id.id
+                record.related_company_id = related_company_id[0].id
 
     @api.constrains("company_ids")
     def _constrains_partner_company_ids(self):
--- energy_communities_service_invoicing/models/contract_template.py
+++ energy_communities_service_invoicing/models/contract_template.py
@@ -8,18 +8,23 @@ class ContractTemplate(models.Model):
     is_pack = fields.Boolean(compute="compute_is_pack", store=True)
 
     def compute_is_pack(self):
-        for record in self:
-            record.is_pack = bool(
-                self.env["product.template"].search(
-                    [
-                        ("property_contract_template_id", "=", record.id),
-                        (
-                            "categ_id",
-                            "=",
-                            self.env.ref(
-                                "energy_communities_service_invoicing.product_category_pack"
-                            ).id,
-                        ),
-                    ]
+        try:
+            categ_id = self.env.ref(
+                "energy_communities_service_invoicing.product_category_pack"
+            ).id
+        except:
+            categ_id = False
+        if categ_id:
+            for record in self:
+                record.is_pack = bool(
+                    self.env["product.template"].search(
+                        [
+                            ("property_contract_template_id", "=", record.id),
+                            (
+                                "categ_id",
+                                "=",
+                                categ_id,
+                            ),
+                        ]
+                    )
                 )
-            )
