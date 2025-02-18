1b1bd6828f310da148b1766070faede34cc935cf
daniquilez <dani.quilez@gmail.com>
2025-02-18 10:56:43 +0100

    [IMP] ✨ ACLs and Record rules for some needed models on service invoicing

---
+++ energy_communities_service_invoicing/models/contract_line_formula.py
@@ -0,0 +1,10 @@
+from odoo import api, fields, models
+
+
+class ContractLineFormula(models.Model):
+    _name = "contract.line.qty.formula"
+    _inherit = "contract.line.qty.formula"
+
+    company_id = fields.Many2one(
+        "res.company", default=lambda self: self.env.company, required=True
+    )
