a306d246b2eb07eabebf60bd6b75907379732496
daniquilez <dani.quilez@gmail.com>
2025-02-18 10:56:43 +0100

    [IMP] ✨ ACLs and Record rules for some needed models on service invoicing

--- energy_communities_service_invoicing/models/__init__.py
+++ energy_communities_service_invoicing/models/__init__.py
@@ -1,6 +1,7 @@
 from . import abstract_contract
 from . import account_move
 from . import contract
+from . import contract_line_formula
 from . import contract_template
 from . import product_template
 from . import sale_order
