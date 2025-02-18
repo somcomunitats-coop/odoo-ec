08e3bac45fbf1bcb107b461a0cb6304f72afb341
daniquilez <dani.quilez@gmail.com>
2025-02-18 12:14:37 +0100

    [IMP] ✨ Contract related services

--- energy_communities_service_invoicing/models/product_template.py
+++ energy_communities_service_invoicing/models/product_template.py
@@ -7,6 +7,22 @@ class ProductTemplate(models.Model):
     property_contract_template_id = fields.Many2one(
         company_dependent=False,
     )
+    related_contract_product_ids = fields.One2many(
+        "product.product",
+        string="Related services",
+        compute="_compute_related_contract_product_ids",
+        store=False,
+    )
+
+    @api.depends("property_contract_template_id")
+    def _compute_related_contract_product_ids(self):
+        for record in self:
+            rel_products = [(5, 0, 0)]
+            record.related_contract_product_ids = rel_products
+            if record.property_contract_template_id:
+                for line in record.property_contract_template_id.contract_line_ids:
+                    rel_products.append((4, line.product_id.id))
+                record.related_contract_product_ids = rel_products
 
     @api.constrains("property_contract_template_id")
     def compute_contract_template_is_pack(self):
