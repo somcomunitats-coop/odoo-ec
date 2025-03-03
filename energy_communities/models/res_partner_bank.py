7ba7b86035a836a936c712bb54c92bba2f0fb115
daniquilez <dani.quilez@gmail.com>
2025-03-03 14:10:42 +0100

    [IMP] ✨ Maintain sync between company and partner company_id _ids

--- energy_communities/models/res_partner_bank.py
+++ energy_communities/models/res_partner_bank.py
@@ -1,4 +1,4 @@
-from odoo import models
+from odoo import api, models
 
 
 class ResPartnerBank(models.Model):
@@ -11,3 +11,9 @@ class ResPartnerBank(models.Model):
             "Account Number must be unique",
         ),
     ]
+
+    # https://github.com/OCA/bank-payment/blob/16.0/account_banking_mandate/models/res_partner_bank.py
+    # TODO: I+D: Check why setting company_id and company_ids on partner triggers bank account company_id constrain
+    # @api.constrains("company_id")
+    # def _company_constrains(self):
+    #     return True
