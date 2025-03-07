1011bb3b734c22efd1162419e1c040022a723185
daniquilez <dani.quilez@gmail.com>
2025-03-07 12:05:54 +0100

    [FIX] 🐛 Call to private method.

--- energy_selfconsumption/models/contract_template.py
+++ energy_selfconsumption/models/contract_template.py
@@ -1,8 +1,9 @@
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 
 PACK_VALUES = [
     ("selfconsumption_pack", _("Selfconsumption Pack")),
 ]
+
 
 class ContractTemplate(models.Model):
     _inherit = "contract.template"
@@ -11,5 +12,8 @@ class ContractTemplate(models.Model):
 
     def custom_compute_pack_type(self):
         super().custom_compute_pack_type()
-        if self.pack_type == 'none':
-            self.set_custom_pack_type("selfconsumption.product_category_selfconsumption_pack", 'selfconsumption_pack')
+        if self.pack_type == "none":
+            self._set_custom_pack_type(
+                "selfconsumption.product_category_selfconsumption_pack",
+                "selfconsumption_pack",
+            )
