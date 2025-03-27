412be085453eefd322d3753767c6f3787c628bba
daniquilez <dani.quilez@gmail.com>
2025-03-27 15:07:46 +0100

    [IMP] ✨ Show company reference on name_get for mail template

--- energy_communities/models/mail_template.py
+++ energy_communities/models/mail_template.py
@@ -5,3 +5,20 @@ class MailTemplate(models.Model):
     _inherit = "mail.template"
 
     company_id = fields.Many2one(required=False, default=lambda self: self.env.company)
+
+    def name_get(self):
+        # Prefetch the fields used by the `name_get`, so `browse` doesn't fetch other fields
+        self.browse(self.ids).read(["name", "company_id"])
+        return [
+            (
+                template.id,
+                "%s%s"
+                % (
+                    template.company_id
+                    and "[%s] " % template.company_id.name
+                    or "[GLOBAL] ",
+                    template.name,
+                ),
+            )
+            for template in self
+        ]
