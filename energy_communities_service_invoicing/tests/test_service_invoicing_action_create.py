59b017ef71add94ac83573c727d6a374e3af67cf
Juan Pedro Sánchez <juanpe.sn@gmail.com>
2025-04-16 17:07:04 +0200

    [REF] ♻️  energy_communities_service_invoicing: move `update_old_contract_to_service_invoicing` from energy_selfconsumption
    move `update_old_contract_to_service_invoicing` method from `energy_selfcionsumption` to `ActionCreate` abstract model in
    `energy_communities_service_invoicing` module.
    
    The reason to do this, is beacuse test that migration method was being
    so hard to test. In order to simplify this, we create an especific class
    `ActionCreate` and also split the functionality of
    `update_old_contract_to_service_invoicing` in small methods that one can
    test now

--- energy_communities_service_invoicing/tests/test_service_invoicing_action_create.py
+++ energy_communities_service_invoicing/tests/test_service_invoicing_action_create.py
@@ -1,4 +1,3 @@
-import re
 import unittest
 
 from dateutil.relativedelta import relativedelta
@@ -13,69 +12,13 @@ from odoo.addons.energy_communities.utils import (
 
 
 @tagged("-at_install", "post_install")
-class TestSeriveInvoicingActionCreate(TransactionCase):
+class TestActionCreate(TransactionCase):
     def setUp(self):
         super().setUp()
+        self.ActionCreate = self.env[
+            "energy_communities_service_invoicing.action_create"
+        ]
         self.maxDiff = None
-
-    def _get_pricelist_for_contract(self, contract, selfconsumption_project):
-        price = contract.contract_line_ids[0].price_unit
-        product_template = self.env.ref(
-            "energy_selfconsumption.product_product_energy_delivered_product_template"
-        )
-        pricelist = self.env["product.pricelist"].create(
-            {
-                "name": f"{selfconsumption_project.name} {selfconsumption_project.invoicing_mode} Selfconsumption Pricelist",
-                "company_id": selfconsumption_project.company_id.id,
-                "currency_id": selfconsumption_project.company_id.currency_id.id,
-                "discount_policy": "without_discount",
-                "item_ids": [
-                    (
-                        0,
-                        0,
-                        {
-                            "base": "standard_price",
-                            "product_tmpl_id": product_template.id,
-                            "product_id": product_template.product_variant_id.id,
-                            "compute_price": "fixed",
-                            "fixed_price": price,
-                            "categ_id": self.env.ref(
-                                "energy_selfconsumption.product_category_selfconsumption_service"
-                            ).id,
-                        },
-                    )
-                ],
-            }
-        )
-        return pricelist
-
-    def _get_payment_mode(self, selfconsumption_project):
-        return self.env["account.payment.mode"].search(
-            [
-                ("company_id", "=", selfconsumption_project.company_id.id),
-                ("payment_type", "=", "inbound"),
-            ]
-        )
-
-    def _get_journal(self, selfconsumption_project):
-        return self.env["account.journal"].search(
-            [
-                ("company_id", "=", selfconsumption_project.company_id.id),
-                ("type", "=", "sale"),
-            ],
-            limit=1,
-        )
-
-    def _get_supply_point(self, contract):
-        supply_point_id = None
-        contract_cups = contract.contract_line_ids[0].name
-        match = re.search(r"CUPS:\s*(\S+)", contract_cups)
-        if match:
-            contract_cups = match.group(1)
-            supply_point_id = self.env["energy_selfconsumption.supply_point"].search(
-                [("code", "=", contract_cups)]
-            )
-        return supply_point_id
 
     @unittest.skip("skip")
     def test_demo_data_creation_ok(self):
@@ -99,70 +42,54 @@ class TestSeriveInvoicingActionCreate(TransactionCase):
             "Platform pack contract template",
         )
 
-    # @unittest.skip("skip")
-    def test_contract_recurrency_ok(self):
-        # Given a project with one contract
+    def test_update_selfconsumption_project(self):
+        # Given a project
         selfconsumption_project = self.env[
             "energy_selfconsumption.selfconsumption"
         ].search([("id", "=", 29)])
-        contract = self.env["contract.contract"].search([("id", "=", 66)])[0]
-        # and a pricelist
-        pricelist = self._get_pricelist_for_contract(contract, selfconsumption_project)
-        # and a product pack
-        pack = self.env.ref(
-            "energy_selfconsumption.product_product_energy_delivered_product_pack_template"
+        # and a set of contracts
+        contracts = selfconsumption_project.get_contracts()
+
+        # when we update that project and contracts
+        res = self.ActionCreate.update_selfconsumption_project(
+            selfconsumption_project, contracts
         )
-        # and a payment mode
-        payment_mode = self._get_payment_mode(selfconsumption_project)
-        # and a journal_id
-        journal_id = self._get_journal(selfconsumption_project)
-        # and a supply_point
-        supply_point_id = self._get_supply_point(contract)
 
-        with contract_utils(self.env, contract) as component:
-            if contract.date_end:
-                close_date = contract.date_end
-                # TODO: Verify why not all lines preserve date_end after this execution
-                component.set_contract_status_closed(close_date)
-                self.assertEqual(contract.contract_line_ids[0].date_end, close_date)
-                self.assertEqual(contract.date_end, close_date)
-                self.assertEqual(contract.status, "closed")
-                self.assertFalse(bool(contract.recurring_next_date))
-            else:
-                execute_date = (
-                    contract.last_date_invoiced
-                    if contract.last_date_invoiced
-                    else contract.date_start
-                )
-                component.set_contract_status_closed(execute_date)
-                new_contract_id = component.reopen(
-                    execute_date,
-                    pricelist,
-                    pack,
-                    False,
-                    payment_mode[0],
-                    {
-                        "selfconsumption_id": selfconsumption_project.id,
-                        "supply_point_id": supply_point_id.id,
-                        "recurring_interval": contract.recurring_interval,
-                        "recurring_rule_type": contract.recurring_rule_type,
-                        "recurring_invoicing_type": contract.recurring_invoicing_type,
-                        "last_date_invoiced": contract.last_date_invoiced,
-                        "journal_id": journal_id.id,
-                        "project_id": selfconsumption_project.id,
-                        "company_id": selfconsumption_project.company_id.id,
-                    },
-                )
-                self.assertTrue(bool(contract.recurring_next_date))
-                self.assertTrue(bool(new_contract_id.recurring_next_date))
+        # then no errors happend
+        self.assertIsNone(res)
 
-                with contract_utils(self.env, service_invoicing_id) as component:
-                    component.set_contract_status_active(contract.last_date_invoiced)
+    def test_reclose_contract__ok(self):
+        # given a contract that has already a date_end
+        contract = self.env["contract.contract"].search([("id", "=", 66)])[0]
+        # a close_date
+        close_date = contract.date_end
+        # when we reclose that contract
+        self.ActionCreate.reclose_contract(contract)
 
-                # Then
-                self.assertEqual(new_contract_id.status, "in_progress")
-                self.assertEqual(new_contract_id.date_start, execution_date)
-                self.assertEqual(contract.date_end, contract.last_date_invoiced)
+        # then
+        self.assertEqual(contract.contract_line_ids[0].date_end, close_date)
+        self.assertEqual(contract.date_end, close_date)
+        self.assertEqual(contract.status, "closed")
+        self.assertFalse(bool(contract.recurring_next_date))
+
+    @unittest.skip("Find a contract without date_end to test")
+    def test_reopen_contract_without_date_end__ok(self):
+        # Given a project
+        selfconsumption_project = self.env[
+            "energy_selfconsumption.selfconsumption"
+        ].search([("id", "=", 29)])
+        # with a contract that has already a date_end
+        contract = None  # self.env["contract.contract"].search([("id", "=", 66)])[0]
+
+        # when we reopen that contract
+        self.ActionCreate.reopen_contract_without_date_end(
+            selfconsumption_project, contract
+        )
+
+        # then
+        self.assertTrue(bool(contract.recurring_next_date))
+        self.assertTrue(bool(new_contract_id.recurring_next_date))
+
         # self.assertEqual(
         #     service_invoicing_id.recurring_next_date,
         #     contract.recurring_next_date
