a43cdf5440480710f2fdd385d1a43db7d04016e3
Álvaro García <alvarogruiz8@gmail.com>
2025-06-18 12:42:11 +0200

    [REL] 🏗️  New release selfconsumption
    Author: Alvaro Garcia <alvaro.garcia@somit.coop>

--- energy_selfconsumption/__manifest__.py
+++ energy_selfconsumption/__manifest__.py
@@ -9,7 +9,7 @@
     "author": "Coopdevs Treball SCCL & Som Energia SCCL",
     "website": "https://git.coopdevs.org/coopdevs/comunitats-energetiques/odoo-ce",
     "category": "Customizations",
-    "version": "16.0.0.4.4",
+    "version": "16.0.0.4.5",
     "license": "AGPL-3",
     "depends": [
         "base",
