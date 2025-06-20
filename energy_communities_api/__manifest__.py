378162b27106ab37bd0118db58b5896a9d66d8e7
daniquilez <dani.quilez@gmail.com>
2025-06-20 10:44:06 +0200

    [REL] 🏗️ energy_communities 16.0.0.4.7 and energy_communities_api 16.0.0.3.2

--- energy_communities_api/__manifest__.py
+++ energy_communities_api/__manifest__.py
@@ -9,7 +9,7 @@
     "author": "Coopdevs Treball SCCL & Som Energia SCCL & SomIT",
     "website": "https://git.coopdevs.org/coopdevs/comunitats-energetiques/odoo-ce",
     "category": "Customizations",
-    "version": "16.0.0.3.1",
+    "version": "16.0.0.3.2",
     "license": "AGPL-3",
     "depends": [
         "auth_jwt",
