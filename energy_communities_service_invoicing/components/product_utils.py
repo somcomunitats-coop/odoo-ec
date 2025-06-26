7d31a8452368a97acadf56649a338efd7b94953b
daniquilez <dani.quilez@gmail.com>
2025-06-26 15:34:20 +0200

    [IMP] ✨ Base Product Creation

--- energy_communities_service_invoicing/components/product_utils.py
+++ energy_communities_service_invoicing/components/product_utils.py
@@ -1,9 +1,67 @@
+from typing import List
+
 from odoo.addons.component.core import Component
+from odoo.addons.product.models.product_template import ProductTemplate
+
+from ..schemas import (
+    BaseProductCreationData,
+    PackProductCreationData,
+    ProductCreationResult,
+    ServiceProductCreationData,
+)
 
 
 class ProductUtils(Component):
     _inherit = "product.utils"
 
-    def create_product(self):
-        __import__("ipdb").set_trace()
-        print("HERE")
+    def create_products(
+        self,
+        pack_product_creation_data: PackProductCreationData = False,
+        service_product_creation_data_list: List[ServiceProductCreationData] = False,
+    ) -> ProductCreationResult:
+        pack_product_template = False
+        service_product_template_list = []
+        # PACK PRODUCT
+        if pack_product_creation_data:
+            pack_product_template = self._create_pack_product(
+                pack_product_creation_data
+            )
+        # SERVICE PRODUCTS
+        if service_product_creation_data_list:
+            for service_product_creation_data in service_product_creation_data_list:
+                service_product_template_list.append(
+                    self._create_base_product(
+                        BaseProductCreationData(
+                            company_id=service_product_creation_data.company_id,
+                            categ_id=service_product_creation_data.categ_id,
+                            name=service_product_creation_data.name,
+                            description_sale=service_product_creation_data.description_sale,
+                            list_price=service_product_creation_data.list_price,
+                            taxes_id=service_product_creation_data.taxes_id,
+                        )
+                    )
+                )
+        return ProductCreationResult(
+            pack_product_template=pack_product_template,
+            service_product_template_list=service_product_template_list,
+        )
+
+    def _create_pack_product(
+        self, pack_product_creation_data: PackProductCreationData
+    ) -> ProductTemplate:
+        pack_product = self._create_base_product(
+            BaseProductCreationData(
+                company_id=pack_product_creation_data.company_id,
+                categ_id=pack_product_creation_data.categ_id,
+                name=pack_product_creation_data.name,
+                description_sale=pack_product_creation_data.description_sale,
+                list_price=pack_product_creation_data.list_price,
+                taxes_id=pack_product_creation_data.taxes_id,
+            )
+        )
+        return pack_product
+
+    def _create_base_product(
+        self, product_creation_data: BaseProductCreationData
+    ) -> ProductTemplate:
+        return self.env["product.template"].create(product_creation_data.dict())
--- energy_communities_service_invoicing/tests/test_product_utils_component.py
+++ energy_communities_service_invoicing/tests/test_product_utils_component.py
@@ -4,8 +4,14 @@ from odoo.tests import tagged
 from odoo.tests.common import TransactionCase
 
 from odoo.addons.energy_communities.utils import product_utils
+from odoo.addons.product.models.product_template import ProductTemplate
 
-from ..schemas import PackProductCreationData, ServiceProductCreationData
+from ..schemas import (
+    BaseProductCreationData,
+    PackProductCreationData,
+    ProductCreationResult,
+    ServiceProductCreationData,
+)
 
 PackProductDataTestingCase = namedtuple(
     "PackProductDataTestingCase", list(PackProductCreationData.model_fields.keys())
@@ -21,6 +27,7 @@ ProductUtilsTestingCase = namedtuple(
 
 _PACK_PRODUCT_TESTING_CASES = {
     "fixed_prepaid_recurring_fee_pack": PackProductDataTestingCase(
+        "Community 1",
         "energy_communities.product_category_recurring_fee_pack",
         "Recurring fee pack test 1",
         "Recurring fee pack test 1 long description",
@@ -37,6 +44,7 @@ _PACK_PRODUCT_TESTING_CASES = {
 }
 _SERVICE_PRODUCT_TESTING_CASES = {
     "recurring_fee_service": ServiceProductDataTestingCase(
+        "Community 1",
         "energy_communities.product_category_recurring_fee_service",
         "Recurring fee service test 1",
         "Recurring fee service test 1 long description",
@@ -69,8 +77,57 @@ class TestProductUtilsComponent(TransactionCase):
 
     def _pack_product_creator_wizard_case(self, case):
         data = self._prepare_all_case_data(case)
-        product_category = self.env.ref(case.pack_product_case.categ_id)
-        self.assertTrue(bool(product_category))
+
+        pack_data = data["pack"]
+        service_data = data["services"][0]
+
+        # ASSERT: Products we're about to create doesn't exist
+        self.assertFalse(
+            bool(self.env["product.template"].search([("name", "=", pack_data.name)]))
+        )
+        self.assertFalse(
+            bool(
+                self.env["product.template"].search([("name", "=", service_data.name)])
+            )
+        )
+
+        # WORKFLOW: Create products
+        with product_utils(self.env) as component:
+            result = component.create_products(data["pack"], data["services"])
+
+        # ASSERT: Creation returns a ProductCreationResult
+        self.assertIsInstance(result, ProductCreationResult)
+        # ASSERT: Base Product Data
+        # for pack
+        self._assert_base_product_data(result.pack_product_template, pack_data)
+        # for services
+        self.assertTrue(bool(result.service_product_template_list))
+        for service_product_template in result.service_product_template_list:
+            self._assert_base_product_data(service_product_template, service_data)
+
+    def _assert_base_product_data(self, product_template, test_data):
+        self.assertTrue(bool(product_template))
+        self.assertIsInstance(product_template, ProductTemplate)
+        for field in BaseProductCreationData.model_fields.keys():
+            if field == "company_id":
+                self.assertEqual(
+                    getattr(product_template, field),
+                    self.env["res.company"].browse(getattr(test_data, field)),
+                )
+            elif field == "categ_id":
+                self.assertEqual(
+                    getattr(product_template, field),
+                    self.env["product.category"].browse(getattr(test_data, field)),
+                )
+            elif field == "taxes_id":
+                product_tax_creation_dict = []
+                for tax in getattr(product_template, field):
+                    product_tax_creation_dict.append((4, tax.id))
+                self.assertEqual(product_tax_creation_dict, getattr(test_data, field))
+            else:
+                self.assertEqual(
+                    getattr(product_template, field), getattr(test_data, field)
+                )
 
     def _prepare_all_case_data(self, case):
         return {
@@ -79,11 +136,13 @@ class TestProductUtilsComponent(TransactionCase):
                 PackProductDataTestingCase,
                 PackProductCreationData,
             ),
-            "service": self._prepare_one_case_data(
-                case.service_product_case,
-                ServiceProductDataTestingCase,
-                ServiceProductCreationData,
-            ),
+            "services": [
+                self._prepare_one_case_data(
+                    case.service_product_case,
+                    ServiceProductDataTestingCase,
+                    ServiceProductCreationData,
+                )
+            ],
         }
 
     def _prepare_one_case_data(
@@ -94,7 +153,15 @@ class TestProductUtilsComponent(TransactionCase):
             for field in testing_case_class._fields:
                 data_val = getattr(product_case, field)
                 if data_val:
-                    if field == "categ_id":
+                    if field == "company_id":
+                        params[field] = (
+                            self.env["res.company"]
+                            .search([("name", "=", data_val)], limit=1)
+                            .id
+                            if data_val
+                            else None
+                        )
+                    elif field == "categ_id":
                         params[field] = self.env.ref(data_val).id
                     elif field == "taxes_id":
                         params[field] = self._prepare_refs_data(data_val)
@@ -106,5 +173,5 @@ class TestProductUtilsComponent(TransactionCase):
     def _prepare_refs_data(self, refs):
         vals = []
         for ref in refs:
-            vals.append((4, self.env.ref(ref)))
+            vals.append((4, self.env.ref(ref).id))
         return vals
