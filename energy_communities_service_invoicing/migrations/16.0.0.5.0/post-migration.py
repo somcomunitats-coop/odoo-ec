e30e41effa3851bb55336548a309923b28a7d296
daniquilez <dani.quilez@gmail.com>
2025-07-08 12:06:29 +0200

    [IMP] ✨ Correctly compute is_pack and is_service on new version activation

---
+++ energy_communities_service_invoicing/migrations/16.0.0.5.0/post-migration.py
@@ -0,0 +1,15 @@
+import logging
+
+from odoo import SUPERUSER_ID, api
+
+logger = logging.getLogger(__name__)
+from odoo.exceptions import UserError
+
+
+def migrate(cr, version):
+    logger.info("Running post migration {}".format(version))
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    product_categories = env["product.category"].search([])
+    for product_category in product_categories:
+        product_category._compute_is_pack()
+        product_category._compute_is_service()
