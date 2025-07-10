d28e6fab96470ec9b9a3199e42003f69d3b4282f
daniquilez <dani.quilez@gmail.com>
2025-07-10 12:46:32 +0200

    [IMP] ✨ Create config tariff on all companies

--- energy_communities_service_invoicing/migrations/16.0.0.5.0/post-migration.py
+++ energy_communities_service_invoicing/migrations/16.0.0.5.0/post-migration.py
@@ -6,11 +6,30 @@ logger = logging.getLogger(__name__)
 from odoo.exceptions import UserError
 
 
-def migrate(cr, version):
-    logger.info("Running post migration {}".format(version))
-    env = api.Environment(cr, SUPERUSER_ID, {})
+def _compute_product_category_flags(env):
     product_categories = env["product.category"].search([])
     for product_category in product_categories:
         product_category._compute_is_pack()
         product_category._compute_is_pack_service()
         product_category._compute_is_assignable_pack_to_partner()
+
+
+def _create_companies_config_tariff(env):
+    companies = env["res.company"].search([])
+    for company in companies:
+        company_pricelist = env["product.pricelist"].create(
+            {
+                "name": "{} pricelist".format(company.name),
+                "currency_id": env.ref("base.EUR").id,
+                "company_id": company.id,
+            }
+        )
+        company.write({"pricelist_id": company_pricelist.id})
+
+
+def migrate(cr, version):
+    logger.info("Running post migration {}".format(version))
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    _compute_product_category_flags(env)
+    _create_companies_config_tariff(env)
+    logger.info("Post migration {} executed".format(version))
