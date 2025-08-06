a45a3d4e5a9c196673b522016ef0ebac430b140a
daniquilez <dani.quilez@gmail.com>
2025-08-06 14:40:41 +0200

    [IMP] ✨ Define company pricelist on mcec company creation

--- energy_communities_service_invoicing/tests/test_multicompany_easy_creation.py
+++ energy_communities_service_invoicing/tests/test_multicompany_easy_creation.py
@@ -6,6 +6,10 @@ class TestMultiCompanyEasyCreation(common.TransactionCase):
     def setUp(self):
         super().setUp()
         self.maxDiff = None
+
+    @classmethod
+    def setUpClass(self):
+        super().setUpClass()
         self.expected_users = [
             # coordinator admins from coordinator company
             self.env.ref("energy_communities.res_users_admin_coordinator_1_demo"),
@@ -16,6 +20,35 @@ class TestMultiCompanyEasyCreation(common.TransactionCase):
             self.env.ref("base.public_user"),
         ]
         self.coord_company = self.env.ref("energy_communities.coordinator_company_1")
+        # avoid public data creation on testing
+        # execute async methods directly
+        wizard_data_update = {
+            "create_landing": False,
+            "create_place": False,
+            "hook_cron": False,
+        }
+        self.cooperative_creation_crm_lead = self.env.ref(
+            "energy_communities_crm.crm_lead_company_creation_demo_1"
+        )
+        creation_data = (
+            self.cooperative_creation_crm_lead._get_default_community_wizard()
+        )
+        creation_data.update(wizard_data_update)
+        self.new_cooperative_wizard = self.env[
+            "account.multicompany.easy.creation.wiz"
+        ].create(creation_data)
+        self.new_cooperative_wizard.action_accept()
+        self.coop_company = self.new_cooperative_wizard.new_company_id
+        self.nonprofit_creation_crm_lead = self.env.ref(
+            "energy_communities_crm.crm_lead_company_creation_demo_2"
+        )
+        creation_data = self.nonprofit_creation_crm_lead._get_default_community_wizard()
+        creation_data.update(wizard_data_update)
+        self.new_nonprofit_wizard = self.env[
+            "account.multicompany.easy.creation.wiz"
+        ].create(creation_data)
+        self.new_nonprofit_wizard.action_accept()
+        self.nonprofit_company = self.new_nonprofit_wizard.new_company_id
 
     def test__company_creation_relation_users_list(self):
         self.assertEqual(
@@ -28,63 +61,52 @@ class TestMultiCompanyEasyCreation(common.TransactionCase):
         )
 
     def test__wizard_creation_ok(self):
-        creation_crm_lead = self.env.ref(
-            "energy_communities_crm.crm_lead_company_creation_demo_1"
-        )
-        wizard = self._get_company_creation_wizard(creation_crm_lead)
-        self.assertEqual(wizard.parent_id, self.coord_company)
-        self.assertEqual(wizard.crm_lead_id, creation_crm_lead)
+        self.assertEqual(self.new_cooperative_wizard.parent_id, self.coord_company)
         self.assertEqual(
-            wizard.chart_template_id,
+            self.new_cooperative_wizard.crm_lead_id, self.cooperative_creation_crm_lead
+        )
+        self.assertEqual(
+            self.new_cooperative_wizard.chart_template_id,
             self.env.ref("l10n_es.account_chart_template_pymes"),
         )
         self.assertEqual(
-            wizard.default_sale_tax_id,
+            self.new_cooperative_wizard.default_sale_tax_id,
             self.env.ref("l10n_es.account_tax_template_s_iva21s"),
         )
         self.assertEqual(
-            wizard.default_purchase_tax_id,
+            self.new_cooperative_wizard.default_purchase_tax_id,
             self.env.ref("l10n_es.account_tax_template_p_iva21_bc"),
         )
-        self.assertFalse(wizard.create_user)
-        self.assertFalse(wizard.creation_partner)
-        creation_crm_lead = self.env.ref(
-            "energy_communities_crm.crm_lead_company_creation_demo_2"
-        )
-        wizard = self._get_company_creation_wizard(creation_crm_lead)
+        self.assertFalse(self.new_cooperative_wizard.create_user)
+        self.assertFalse(self.new_cooperative_wizard.creation_partner)
         self.assertEqual(
-            wizard.chart_template_id,
+            self.new_nonprofit_wizard.chart_template_id,
             self.env.ref("l10n_es.account_chart_template_assoc"),
         )
 
     def test__community_creation_ok(self):
-        creation_crm_lead = self.env.ref(
-            "energy_communities_crm.crm_lead_company_creation_demo_1"
-        )
-        wizard = self._get_company_creation_wizard(creation_crm_lead)
-        wizard.action_accept()
         companies = self.env["res.company"].search(
             [("parent_id", "=", self.coord_company.id)]
         )
-        # ASSERT: Now we have 3 communities
-        self.assertEqual(len(companies), 3)
+        # ASSERT: Now we have 4 communities
+        self.assertEqual(len(companies), 4)
         for company in companies:
             self.assertEqual(company.hierarchy_level, "community")
-        # ASSERT: New company has been created
-        self.assertTrue(bool(wizard.new_company_id))
+        # ASSERT: New companies has been created
+        self.assertTrue(bool(self.coop_company))
+        self.assertTrue(bool(self.nonprofit_company))
 
     def test__users_and_partners_configuration_ok(self):
-        creation_crm_lead = self.env.ref(
-            "energy_communities_crm.crm_lead_company_creation_demo_1"
-        )
-        wizard = self._get_company_creation_wizard(creation_crm_lead)
-        wizard.action_accept()
+        self._test__users_and_partners_configuration_ok_case(self.coop_company)
+        self._test__users_and_partners_configuration_ok_case(self.nonprofit_company)
+
+    def _test__users_and_partners_configuration_ok_case(self, new_company):
         for user in self.expected_users:
             # ASSERT: community is defined on destination user company_ids
             self.assertTrue(
                 bool(
                     user.company_ids.filtered(
-                        lambda company: company.id == wizard.new_company_id.id
+                        lambda company: company.id == new_company.id
                     )
                 )
             )
@@ -92,7 +114,7 @@ class TestMultiCompanyEasyCreation(common.TransactionCase):
             self.assertTrue(
                 bool(
                     user.partner_id.company_ids.filtered(
-                        lambda company: company.id == wizard.new_company_id.id
+                        lambda company: company.id == new_company.id
                     )
                 )
             )
@@ -103,7 +125,7 @@ class TestMultiCompanyEasyCreation(common.TransactionCase):
                     lambda role_line: (
                         role_line.role_id.id
                         == self.env.ref("energy_communities.role_ce_manager").id
-                        and role_line.company_id.id == wizard.new_company_id.id
+                        and role_line.company_id.id == new_company.id
                     )
                 )
             )
@@ -111,15 +133,15 @@ class TestMultiCompanyEasyCreation(common.TransactionCase):
         # ASSERT: new company related partner has new company assigned
         self.assertTrue(
             bool(
-                wizard.new_company_id.partner_id.company_ids.filtered(
-                    lambda company: company.id == wizard.new_company_id.id
+                new_company.partner_id.company_ids.filtered(
+                    lambda company: company.id == new_company.id
                 )
             )
         )
         # ASSERT: new company  related partner has platform company assigned
         self.assertTrue(
             bool(
-                wizard.new_company_id.partner_id.company_ids.filtered(
+                new_company.partner_id.company_ids.filtered(
                     lambda company: company.id == self.env.ref("base.main_company").id
                 )
             )
@@ -127,27 +149,28 @@ class TestMultiCompanyEasyCreation(common.TransactionCase):
         # ASSERT: new company related partner has new company parent assigned
         self.assertTrue(
             bool(
-                wizard.new_company_id.partner_id.company_ids.filtered(
-                    lambda company: company.id == wizard.new_company_id.parent_id.id
+                new_company.partner_id.company_ids.filtered(
+                    lambda company: company.id == new_company.parent_id.id
                 )
             )
         )
         # ASSERT: new company parent related partner has new company assigned
         self.assertTrue(
             bool(
-                wizard.new_company_id.parent_id.partner_id.company_ids.filtered(
-                    lambda company: company.id == wizard.new_company_id.id
+                new_company.parent_id.partner_id.company_ids.filtered(
+                    lambda company: company.id == new_company.id
                 )
             )
         )
 
-    def _get_company_creation_wizard(self, creation_crm_lead):
-        data = creation_crm_lead._get_default_community_wizard()
-        # avoid public data creation on testing
-        data.update(
-            {
-                "create_landing": False,
-                "create_place": False,
-            }
+    def test__pricelist_configuration_ok(self):
+        created_pricelist = self.env["product.pricelist"].search(
+            [("company_id", "=", self.coop_company.id)]
         )
-        return self.env["account.multicompany.easy.creation.wiz"].create(data)
+        self.assertEqual(len(created_pricelist), 1)
+        self.assertEqual(self.coop_company.pricelist_id, created_pricelist)
+        created_pricelist = self.env["product.pricelist"].search(
+            [("company_id", "=", self.nonprofit_company.id)]
+        )
+        self.assertEqual(len(created_pricelist), 1)
+        self.assertEqual(self.nonprofit_company.pricelist_id, created_pricelist)
--- energy_communities_service_invoicing/wizards/multicompany_easy_creation.py
+++ energy_communities_service_invoicing/wizards/multicompany_easy_creation.py
@@ -1,5 +1,10 @@
 from odoo import _, api, fields, models
 
+from odoo.addons.energy_communities.utils import (
+    get_successful_popup_message,
+    product_utils,
+)
+
 
 class AccountMulticompanyEasyCreationWiz(models.TransientModel):
     _name = "account.multicompany.easy.creation.wiz"
@@ -10,3 +15,8 @@ class AccountMulticompanyEasyCreationWiz(models.TransientModel):
 
     def thread_action_accept(self):
         super().thread_action_accept()
+        self._create_default_pricelist()
+
+    def _create_default_pricelist(self):
+        with product_utils(self.env) as component:
+            return component.create_company_pricelist(self.new_company_id)
