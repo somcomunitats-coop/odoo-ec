a003278fbde984117aaae4c862a03c9e8bc5a950
Álvaro García <alvarogruiz8@gmail.com>
2025-10-22 16:20:02 +0200

    [IMP] ✨ Clean code
    Author: Alvaro Garcia <alvaro.garcia@somit.coop>

--- energy_communities/static/src/js/switch_company_menu.js
+++ energy_communities/static/src/js/switch_company_menu.js
@@ -1,30 +1,10 @@
 /** @odoo-module **/
 
 import {patch} from "@web/core/utils/patch";
-/*
-import { _t } from "@web/core/l10n/translation";
-
-import { Dropdown } from "@web/core/dropdown/dropdown";
-import { DropdownItem } from "@web/core/dropdown/dropdown_item";
-import { useService } from "@web/core/utils/hooks";
-import { registry } from "@web/core/registry";
-import { browser } from "@web/core/browser/browser";
-import { symmetricalDifference } from "@web/core/utils/arrays";
-
-import { Component, useState } from "@odoo/owl";
-*/
 
 import {SwitchCompanyMenu} from "@web/webclient/switch_company_menu/switch_company_menu";
 
 patch(SwitchCompanyMenu.prototype, "SwitchCompanyMenuCustom", {
-  setup() {
-    this._super(...arguments);
-  },
-
-  toggleCompany(companyId) {
-    this._super(...arguments);
-  },
-
   /**
    * Toggle all companies at once
    * @param {boolean} selectAll - true to select all, false to deselect all
