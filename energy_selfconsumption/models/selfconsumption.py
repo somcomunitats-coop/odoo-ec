e692059cac2fba022c43137decc188eb8ce5c142
Álvaro García <alvarogruiz8@gmail.com>
2025-10-31 12:32:49 +0100

    [FIX] 🐛 Fix assetion for selfconsumption journal and typo on set_new_distribution_table method
    Author: Alvaro Garcia <alvaro.garcia@somit.coop>
    Author: Dani Quilez <dani.quilez@somenergia.coop>

--- energy_selfconsumption/models/selfconsumption.py
+++ energy_selfconsumption/models/selfconsumption.py
@@ -515,7 +515,7 @@ class Selfconsumption(models.Model):
                     "project_id": self.id,
                     "company_id": self.company_id.id,
                     "mandate_id": inscription_id.mandate_id.id
-                    if inscription.mandate_id
+                    if inscription_id.mandate_id
                     else None,
                 }
                 # create service invoicing
