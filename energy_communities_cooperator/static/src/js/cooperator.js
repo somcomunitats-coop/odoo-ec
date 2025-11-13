3cccc5d61d01eb4ae51570e35a22d05c14aaafff
Álvaro García <alvarogruiz8@gmail.com>
2025-11-13 15:28:44 +0100

    [IMP] ✨ Javascript is better than the other version on energy_communities_cooperator
    Author: Alvaro Garcia <alvaro.garcia@somit.coop>

--- energy_communities_cooperator/static/src/js/cooperator.js
+++ energy_communities_cooperator/static/src/js/cooperator.js
@@ -1,53 +1,29 @@
 odoo.define("cooperator.oe_cooperator_inherit", function (require) {
   "use strict";
   $(document).ready(function () {
-    var ajax = require("web.ajax");
-
     $(".oe_cooperator").each(function () {
       var oe_cooperator = this;
 
-      $("#share_product_id").off("change");
-
-      $("#share_product_id").change(function () {
-        var share_product_id = $("#share_product_id").val();
-        ajax
-          .jsonRpc("/subscription/get_share_product", "call", {
-            share_product_id: share_product_id,
-          })
-          .then(function (data) {
-            $("#share_price").text(data[share_product_id].list_price);
-            $("#ordered_parts").val(data[share_product_id].min_qty);
-            if (data[share_product_id].force_min_qty === true) {
-              $("#ordered_parts").data("min", data[share_product_id].min_qty);
-            }
-            $("#ordered_parts").change();
-            var $share_price = $("#share_price").text();
-            $('input[name="total_parts"]').val(
-              $("#ordered_parts").val() * $share_price
-            );
-            $('input[name="total_parts"]').change();
-
-            $(".col-xs-12.col-sm-10.col-md-8.col-lg-6 p>span")[0].textContent =
-              $("#ordered_parts").val() * $share_price;
-          });
+      $("#total_parts").change(function (event) {
+        $("#share_price_span").html($(this).val());
+        return false;
       });
 
-      $("#ordered_parts").off("change");
       // We execute again the change method to take into consideration if the form has been already submitted
       $(oe_cooperator).on("change", "#ordered_parts", function (event) {
         var $share_price = $("#share_price").text();
-        var $ordered_parts = $("#ordered_parts");
         var $link = $(event.currentTarget);
         var quantity = $link[0].value;
+        var total_part = quantity * $share_price;
+        $("#total_parts").val(total_part);
+
+        var $ordered_parts = $("#ordered_parts");
         // This block of code will grab submission value to take it into consideration
         if ($ordered_parts.data("submission_value")) {
           $ordered_parts.val($ordered_parts.data("submission_value"));
           $ordered_parts.data("submission_value", null);
         }
-        var total_part = quantity * $share_price;
-        $("#total_parts").val(total_part);
-
-        $(".col-xs-12.col-sm-10.col-md-8.col-lg-6 p>span")[0].textContent = total_part;
+        $("#total_parts").change();
         return false;
       });
     });
