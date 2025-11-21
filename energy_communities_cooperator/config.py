57c774a173b631d931a573a24411488ffc057e94
Álvaro García <alvarogruiz8@gmail.com>
2025-11-21 14:27:30 +0100

    [WIP] 🚧 Controller and templates views for subcription cooperator request
    Author: Alvaro Garcia <alvaro.garcia@somit.coop>

--- energy_communities_cooperator/config.py
+++ energy_communities_cooperator/config.py
@@ -5,4 +5,16 @@ COOP_VOLUNTARY_SHARE_PRODUCT_CATEG_REF = (
 MAPPING__SUBSCRIPTION_MODE__PRODUCT_CATEG_REF = {
     "member": "cooperator.product_category_company_share",
     "company_member": "cooperator.product_category_company_share",
+    "invited": "cooperator.product_category_company_share",
+    "company_invited": "cooperator.product_category_company_share",
+    "voluntary": "cooperator.product_category_company_share",
+    "company_voluntary": "cooperator.product_category_company_share",
+}
+MAPPING__SUBSCRIPTION_MODE__PAGE_TITLE = {
+    "member": "Become Cooperator",
+    "company_member": "Become Company Cooperator",
+    "invited": "Become Invited",
+    "company_invited": "Become Company Invited",
+    "voluntary": "Become Voluntary",
+    "company_voluntary": "Become Company Voluntary",
 }
