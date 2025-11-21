5d9a233341ff1b1fdf25423b4fdc6d2610e42943
daniquilez <dani.quilez@gmail.com>
2025-11-21 12:39:25 +0100

    [WIP] 🚧 New subscription controller base structure
     Co-authored-by: Alvaro Garcia <alvaro.garcia@somit.coop>

--- energy_communities_cooperator/config.py
+++ energy_communities_cooperator/config.py
@@ -2,3 +2,7 @@ COOP_SHARE_PRODUCT_CATEG_REF = "cooperator.product_category_company_share"
 COOP_VOLUNTARY_SHARE_PRODUCT_CATEG_REF = (
     "energy_communities_cooperator.product_category_company_voluntary_share"
 )
+MAPPING__SUBSCRIPTION_MODE__PRODUCT_CATEG_REF = {
+    "member": "cooperator.product_category_company_share",
+    "company_member": "cooperator.product_category_company_share",
+}
--- energy_communities_cooperator/controllers/website_share_subscription_controller.py
+++ energy_communities_cooperator/controllers/website_share_subscription_controller.py
@@ -2,15 +2,22 @@ from odoo import http
 from odoo.http import request
 from odoo.tools.translate import _
 
+from ..config import MAPPING__SUBSCRIPTION_MODE__PRODUCT_CATEG_REF
+
 
 # http://odoo-ce.local:8069/es/subscription/member/356a192b7913b04c54574d18c28d46e6395428ab/ae7329c979b3cd96086c22cca6217764ab3e50ec
 # http://odoo-ce.local:8069/es/subscription/invited/356a192b7913b04c54574d18c28d46e6395428ab/ae7329c979b3cd96086c22cca6217764ab3e50ec
 # http://odoo-ce.local:8069/es/subscription/voluntary/356a192b7913b04c54574d18c28d46e6395428ab/ae7329c979b3cd96086c22cca6217764ab3e50ec
 # http://odoo-ce.local:8069/es/subscription/company_invited/356a192b7913b04c54574d18c28d46e6395428ab/ae7329c979b3cd96086c22cca6217764ab3e50ec
 # http://odoo-ce.local:8069/es/subscription/company_voluntary/356a192b7913b04c54574d18c28d46e6395428ab/ae7329c979b3cd96086c22cca6217764ab3e50ec
-# http://odoo-ce.local:8069/es/subscription/company_menber/356a192b7913b04c54574d18c28d46e6395428ab/ae7329c979b3cd96086c22cca6217764ab3e50ec
-# http://odoo-ce.local:8069/es/subscription/error/356a192b7913b04c54574d18c28d46e6395428ab/ae7329c979b3cd96086c22cca6217764ab3e50ec
+# http://odoo-ce.local:8069/es/subscription/company_member/356a192b7913b04c54574d18c28d46e6395428ab/ae7329c979b3cd96086c22cca6217764ab3e50ec
 class WebsiteShareSubscriptionController(http.Controller):
+    mode = None
+    company = None
+    product_categ = None
+    product = None
+    values = {}
+
     @http.route(
         [
             "/subscription/<string:mode>/<string:company_ext_id>",
@@ -31,77 +38,66 @@ class WebsiteShareSubscriptionController(http.Controller):
         Returns:
             The rendered page.
         """
-        # Default kwargs
-        mode = False
-        company_ext_id = False
-        product_ext_id = False
-        if "mode" in kwargs.keys():
-            mode = kwargs.get("mode", False)
-        if "company_ext_id" in kwargs.keys():
-            company_ext_id = kwargs.get("company_ext_id", False)
-        if "product_ext_id" in kwargs.keys():
-            product_ext_id = kwargs.get("product_ext_id", False)
+        self._setup_class_defaults(kwargs)
+        self._get_base_values()
+        # Validate the request
+        self._validate_request()
+        if not "global_error" in self.values.keys():
+            self._get_extended_values()
+        # Render the page
+        return self.render_page()
+
+    def _setup_class_defaults(self, kwargs):
+        self.mode = kwargs.get("mode")
         # Get the company and product from the external ID
-        company = self._get_model_from_ext_id(
-            "res.company", "company_external_id", company_ext_id
+        self.company = self._get_model_from_ext_id(
+            "res.company", "company_external_id", kwargs.get("product_ext_id")
         )
+        product_ext_id = False
+        if "product_ext_id" in kwargs.keys():
+            product_ext_id = kwargs.get("product_ext_id")
         # Get the product from the external ID
-        product = self._get_model_from_ext_id(
+        self.product = self._get_model_from_ext_id(
             "product.template", "product_external_id", product_ext_id
         )
-        # Validate the request
-        values = self._validate_request(mode, company, product)
-        # Render the page
-        return self.render(values)
+        self.product_categ = request.env.ref(
+            MAPPING__SUBSCRIPTION_MODE__PRODUCT_CATEG_REF[self.mode]
+        )
 
+    def _get_base_values(self):
+        __import__("ipdb").set_trace()
+        self.values |= {
+            # template_mode =
+            # "page_title": self._get_page_title()
+        }
+
+    def _validate_request(self):
+        # if mode not in ["member", "invited", "company", "company_invited"]:
+        #     values.update({"global_error": True, "error_msgs": ["Invalid mode"]})
+        #     return values
+        # if not company:
+        #     values.update({"global_error": True, "error_msgs": ["Company not found"]})
+        #     return values
+        # if not product:
+        #     values.update({"global_error": True, "error_msgs": ["Product not found"]})
+        #     return values
+        pass
+
+    def _get_extended_values(self):
+        # values.update(
+        #     {
+        #     }
+        # )
+        pass
+
+    def _render_page(self, values):
+        return request.render(
+            "energy_communities_cooperator.website_template_subscription_base",
+            self.values,
+        )
+
+    # getters
     def _get_model_from_ext_id(self, model_name, field_name, ext_id):
         if ext_id:
             return request.env[model_name].sudo().search([(field_name, "=", ext_id)])
         return False
-
-    def _validate_request(self, mode, company, product):
-        values = {}
-        if mode not in ["member", "invited", "company", "company_invited"]:
-            values.update({"model": "error", "error_msgs": ["Invalid mode"]})
-            return values
-        if not company:
-            values.update({"model": "error", "error_msgs": ["Company not found"]})
-            return values
-        if not product:
-            values.update({"model": "error", "error_msgs": ["Product not found"]})
-            return values
-        return self._fill_values(mode, values, company, product)
-
-    def _fill_values(self, mode, values, company, product):
-        values.update(
-            {
-                # General values
-                # Company values
-                "company_id": company.id,
-                "company_name": company.name,
-                "company.address": company.address,
-                "company_city": company.city,
-                "company_state": company.state,
-                "company_zip": company.zip,
-                "company_country": company.country,
-                "company_email": company.email,
-                "company_phone": company.phone,
-                "company_website": company.website,
-                # Product values
-                "product_id": product.id,
-                "product_name": product.name,
-                "product_description": product.description,
-                "product_price": product.list_price,
-                # Mode values
-                "model": mode,
-            }
-        )
-        return values
-
-    def _render_page(self, template_name, values):
-        return request.render(template_name, values)
-
-    def render(self, values):
-        return self._render_page(
-            "energy_communities_cooperator.subcription_data_page", values
-        )
