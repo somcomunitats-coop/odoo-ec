13629194033720ebcce67f6054750a4a80d1b41b
daniquilez <dani.quilez@gmail.com>
2025-11-21 18:42:24 +0100

    [WIP] 🚧 Next steps from subscription controller. Move class variables to values

--- energy_communities_cooperator/config.py
+++ energy_communities_cooperator/config.py
@@ -1,3 +1,5 @@
+from odoo.tools.translate import _
+
 COOP_SHARE_PRODUCT_CATEG_REF = "cooperator.product_category_company_share"
 COOP_VOLUNTARY_SHARE_PRODUCT_CATEG_REF = (
     "energy_communities_cooperator.product_category_company_voluntary_share"
@@ -10,11 +12,59 @@ MAPPING__SUBSCRIPTION_MODE__PRODUCT_CATEG_REF = {
     "voluntary": "cooperator.product_category_company_share",
     "company_voluntary": "cooperator.product_category_company_share",
 }
-MAPPING__SUBSCRIPTION_MODE__PAGE_TITLE = {
-    "member": "Become Cooperator",
-    "company_member": "Become Company Cooperator",
-    "invited": "Become Invited",
-    "company_invited": "Become Company Invited",
-    "voluntary": "Become Voluntary",
-    "company_voluntary": "Become Company Voluntary",
+MAPPING__SUBSCRIPTION_MODE__DEFAULT_PAGE_TITLE = {
+    "member": _("Become Cooperator of {company_name}") + _("member_eu_ES"),
+    "company_member": _("Become Cooperator of {company_name}") + _("member_eu_ES"),
+    "invited": _("Become Invited of {company_name}") + _("invited_eu_ES"),
+    "company_invited": _("Become Invited of {company_name}") + _("invited_eu_ES"),
+    "voluntary": _("Voluntary Share of {company_name}") + _("vol_share_eu_es"),
+    "company_voluntary": _("Voluntary Share of {company_name}") + _("vol_share_eu_es"),
+}
+MAPPING__SUBSCRIPTION_MODE__DEFAULT_PAGE_HEADLINE = {
+    "member": _("Become Cooperator Headline"),
+    "company_member": _("Become Company Cooperator Headline"),
+    "invited": _("Become Invited Headline"),
+    "company_invited": _("Become Company Invited Headline"),
+    "voluntary": _("Become Voluntary Headline"),
+    "company_voluntary": _("Become Company Voluntary Headline"),
+}
+MAPPING__SUBSCRIPTION_MODE__DEFAULT_PAGE_HEADLINE_FIXED_TRANSFER = {
+    "member": _(
+        "<p>To be a member you must fulfill this form and lateron proceed to pay the initial share of <span id='prodPrice'>{product_price}</span>€ by follow the steps you will receive by email.</p>"
+    ),
+    "company_member": _(
+        "<p>To be a member you must fulfill this form and lateron proceed to pay the initial share of <span id='prodPrice'>{product_price}</span>€ by follow the steps you will receive by email.</p>"
+    ),
+    "invited": _(
+        "<p>To be a member you must fulfill this form and lateron proceed to pay the initial share of <span id='prodPrice'>{product_price}</span>€ by follow the steps you will receive by email.</p>"
+    ),
+    "company_invited": _(
+        "<p>To be a member you must fulfill this form and lateron proceed to pay the initial share of <span id='prodPrice'>{product_price}</span>€ by follow the steps you will receive by email.</p>"
+    ),
+    "voluntary": _(
+        "<p>To be a member you must fulfill this form and lateron proceed to pay the initial share of <span id='prodPrice'>{product_price}</span>€ by follow the steps you will receive by email.</p>"
+    ),
+    "company_voluntary": _(
+        "<p>To be a member you must fulfill this form and lateron proceed to pay the initial share of <span id='prodPrice'>{product_price}</span>€ by follow the steps you will receive by email.</p>"
+    ),
+}
+MAPPING__SUBSCRIPTION_MODE__DEFAULT_PAGE_HEADLINE_FIXED_SEPA = {
+    "member": _(
+        "<p>To join, you must first fill out this form where we ask for a bank account and authorization to issue a bank receipt to collect the initial mandatory financial contribution of <span id=prodPrice'>{product_price}</span>€</p>"
+    ),
+    "company_member": _(
+        "<p>To join, you must first fill out this form where we ask for a bank account and authorization to issue a bank receipt to collect the initial mandatory financial contribution of <span id=prodPrice'>{product_price}</span>€</p>"
+    ),
+    "invited": _(
+        "<p>To join, you must first fill out this form where we ask for a bank account and authorization to issue a bank receipt to collect the initial mandatory financial contribution of <span id=prodPrice'>{product_price}</span>€</p>"
+    ),
+    "company_invited": _(
+        "<p>To join, you must first fill out this form where we ask for a bank account and authorization to issue a bank receipt to collect the initial mandatory financial contribution of <span id=prodPrice'>{product_price}</span>€</p>"
+    ),
+    "voluntary": _(
+        "<p>To join, you must first fill out this form where we ask for a bank account and authorization to issue a bank receipt to collect the initial mandatory financial contribution of <span id=prodPrice'>{product_price}</span>€</p>"
+    ),
+    "company_voluntary": _(
+        "<p>To join, you must first fill out this form where we ask for a bank account and authorization to issue a bank receipt to collect the initial mandatory financial contribution of <span id=prodPrice'>{product_price}</span>€</p>"
+    ),
 }
--- energy_communities_cooperator/controllers/website_share_subscription_controller.py
+++ energy_communities_cooperator/controllers/website_share_subscription_controller.py
@@ -3,7 +3,10 @@ from odoo.http import request
 from odoo.tools.translate import _
 
 from ..config import (
-    MAPPING__SUBSCRIPTION_MODE__PAGE_TITLE,
+    MAPPING__SUBSCRIPTION_MODE__DEFAULT_PAGE_HEADLINE,
+    MAPPING__SUBSCRIPTION_MODE__DEFAULT_PAGE_HEADLINE_FIXED_SEPA,
+    MAPPING__SUBSCRIPTION_MODE__DEFAULT_PAGE_HEADLINE_FIXED_TRANSFER,
+    MAPPING__SUBSCRIPTION_MODE__DEFAULT_PAGE_TITLE,
     MAPPING__SUBSCRIPTION_MODE__PRODUCT_CATEG_REF,
 )
 
@@ -15,12 +18,24 @@ from ..config import (
 # http://odoo-ce.local:8069/es/subscription/company_voluntary/356a192b7913b04c54574d18c28d46e6395428ab/ae7329c979b3cd96086c22cca6217764ab3e50ec
 # http://odoo-ce.local:8069/es/subscription/company_member/356a192b7913b04c54574d18c28d46e6395428ab/ae7329c979b3cd96086c22cca6217764ab3e50ec
 class WebsiteShareSubscriptionController(http.Controller):
-    mode = None
-    company = None
-    product_categ = None
-    product_ext_id = None
-    product = None
-    values = {}
+    @http.route(
+        [
+            "/subscription/<string:mode>/<string:company_ext_id>",
+            "/subscription/<string:mode>/<string:company_ext_id>/<string:product_ext_id>",
+        ],
+        type="http",
+        auth="public",
+        website=True,
+        methods=["GET"],
+    )
+    def subscription_page(self, **kwargs):
+        values = self._get_base_values(kwargs)
+        # Validate the request
+        self._validate_request(values)
+        if not "global_error" in values.keys():
+            self._get_page_values(values)
+        # Render the page
+        return self._render_page(values)
 
     @http.route(
         [
@@ -30,83 +45,73 @@ class WebsiteShareSubscriptionController(http.Controller):
         type="http",
         auth="public",
         website=True,
+        methods=["POST"],
     )
-    def subsctiption_page(self, **kwargs):
-        """
-        This function is used to render the subscription page. It is called when the user visits the subscription page.
-        It validates the request and renders the page.
-        Args:
-            mode: The mode of the subscription page.
-            company_ext_id: The external ID of the company.
-            product_ext_id: The external ID of the product.
-        Returns:
-            The rendered page.
-        """
-        self._setup_class_defaults(kwargs)
-        self._get_base_values()
-        # Validate the request
-        self._validate_request()
-        if not "global_error" in self.values.keys():
-            self._get_extended_values()
-        # Render the page
-        return self._render_page()
+    def subscription_submission(self, **kwargs):
+        values = self._get_base_values(kwargs)
+        return self._render_page(values)
 
-    def _setup_class_defaults(self, kwargs):
-        self.mode = kwargs.get("mode")
-        # Get the company and product from the external ID
-        self.company = self._get_model_from_ext_id(
-            "res.company", "company_external_id", kwargs.get("company_ext_id")
-        )
+    def _get_base_values(self, kwargs):
+        mode = kwargs.get("mode")
+        values = {
+            "mode": mode,
+            "company": self._get_model_from_ext_id(
+                "res.company", "company_external_id", kwargs.get("company_ext_id")
+            ),
+            "product_categ": request.env.ref(
+                MAPPING__SUBSCRIPTION_MODE__PRODUCT_CATEG_REF[mode]
+            ),
+        }
         if "product_ext_id" in kwargs.keys():
-            self.product_ext_id = kwargs.get("product_ext_id")
-        # Get the product from the external ID
-        self.product = self._get_model_from_ext_id(
-            "product.template", "product_external_id", self.product_ext_id
-        )
-        self.product_categ = request.env.ref(
-            MAPPING__SUBSCRIPTION_MODE__PRODUCT_CATEG_REF[self.mode]
-        )
+            values["product_ext_id"] = kwargs.get("product_ext_id")
+            # Get the product from the external ID
+            values["product"] = self._get_model_from_ext_id(
+                "product.template", "product_external_id", values["product_ext_id"]
+            )
+        else:
+            values["product_ext_id"] = False
+            values["product"] = (
+                request.env["product.template"]
+                .sudo()
+                .search(
+                    [
+                        ("categ_id", "=", values["product_categ"].id),
+                        ("default_share_product", "=", True),
+                        ("company_id", "=", values["company"].id),
+                    ],
+                    limit=1,
+                )
+            )
+        return values
 
-    def _get_base_values(self):
-        self.values.update(
-            {
-                "form_submit_url": self._get_form_submit_url(),
-                "page_title": self._get_page_title(),
-                "headline_text_1": self._get_headline_text_1(),
-                "headline_text_3": self._get_headline_text_3(),
-                "footer_text": self._get_footer_text(),
-                "mode": self.mode,
-            }
-        )
-
-    def _validate_request(self):
-        if not self.company:
-            self.values.update(
+    def _validate_request(self, values):
+        if not values["company"]:
+            values.update(
                 {"global_error": True, "error_msgs": [_("Company not found")]}
             )
-        if not self.mode:
-            self.values.update(
-                {"global_error": True, "error_msgs": [_("Mode not found")]}
-            )
-        if self.product_ext_id and not self.product:
+        if not values["mode"]:
+            values.update({"global_error": True, "error_msgs": [_("Mode not found")]})
+        if values["product_ext_id"] and not values["product"]:
             self.values.update(
                 {"global_error": True, "error_msgs": [_("Product not found")]}
             )
 
-    def _get_extended_values(self):
-        self.values.update(
+    def _get_page_values(self, values):
+        values.update(
             {
-                "company_name": self.company.name,
-                "currency_symbol": self.company.currency_id.symbol,
-                "product_price": self.product.list_price,
-                "headline_text_2": self._get_headline_text_2(),
+                "page_title": self._get_page_title(values),
+                "page_headline": self._get_page_headline(values),
+                "page_headline_fixed_transfer": self._get_page_headline_fixed_transfer(
+                    values
+                ),
+                "page_headline_fixed_sepa": self._get_page_headline_fixed_sepa(values),
             }
         )
 
-    def _render_page(self):
+    def _render_page(self, values):
         return request.render(
             "energy_communities_cooperator.template_subscription_page",
-            self.values,
+            values,
         )
 
     # getters
@@ -115,37 +120,22 @@ class WebsiteShareSubscriptionController(http.Controller):
             return request.env[model_name].sudo().search([(field_name, "=", ext_id)])
         return False
 
-    def _get_form_submit_url(self):
-        return f"send/subscription/{self.mode}"
-
-    def _get_page_title(self):
-        return _(MAPPING__SUBSCRIPTION_MODE__PAGE_TITLE[self.mode])
-
-    def _get_headline_text_1(self):
-        return _(
-            f"This form allow you to request be member of the community: {self.company.name}."
+    def _get_page_title(self, values):
+        return MAPPING__SUBSCRIPTION_MODE__DEFAULT_PAGE_TITLE[values["mode"]].format(
+            company_name=values["company"].comercial_name
         )
 
-    def _get_headline_text_2(self):
-        if self.product.list_price:
-            if self.values.get(
-                "share_payment_sepa_direct_debit", False
-            ):  # TODO: check if this is correct. This can be for product category share.
-                return _(
-                    f"To join, you must first fill out this form where we ask for a bank account and authorization to issue a bank receipt to collect the initial mandatory financial contribution of <span>{self.product.list_price}</span>{self.company.currency_id.symbol}."
-                )
-            else:
-                return _(
-                    f"To be a member you must fulfill this form and lateron proceed to pay the initial share of <span>{self.product.list_price}</span>{self.company.currency_id.symbol} by follow the steps you will receive by email."
-                )
-        return _("")
-
-    def _get_headline_text_3(self):
-        return _(
-            "Once you are a member you can enjoy the services available from the community and be part of a movement of social and energy model transformation."
+    def _get_page_headline(self, values):
+        return MAPPING__SUBSCRIPTION_MODE__DEFAULT_PAGE_HEADLINE[values["mode"]].format(
+            product_price=str(values["product"].list_price).replace(".", ",")
         )
 
-    def _get_footer_text(self):
-        return _(
-            "Thank you for your interest in becoming a cooperator. We will review your application and get back to you soon."
-        )
+    def _get_page_headline_fixed_transfer(self, values):
+        return MAPPING__SUBSCRIPTION_MODE__DEFAULT_PAGE_HEADLINE_FIXED_TRANSFER[
+            values["mode"]
+        ].format(product_price=str(values["product"].list_price).replace(".", ","))
+
+    def _get_page_headline_fixed_sepa(self, values):
+        return MAPPING__SUBSCRIPTION_MODE__DEFAULT_PAGE_HEADLINE_FIXED_SEPA[
+            values["mode"]
+        ].format(product_price=str(values["product"].list_price).replace(".", ","))
