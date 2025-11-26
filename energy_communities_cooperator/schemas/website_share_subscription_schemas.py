f4b9096d3bc51d174cb494063cc624157845bcd4
Álvaro García <alvarogruiz8@gmail.com>
2025-11-26 13:19:28 +0100

    [WIP] 🚧 Fix names
    Author: Alvaro Garcia <alvaro.garcia@somit.coop>

--- energy_communities_cooperator/schemas/website_share_subscription_schemas.py
+++ energy_communities_cooperator/schemas/website_share_subscription_schemas.py
@@ -1,12 +1,12 @@
+from enum import Enum
 from typing import Optional
 
 from pydantic import BaseModel
 
-from odoo.addons.base.models.res_company import ResCompany
+from odoo.addons.base.models.res_company import Company
 from odoo.addons.product.models.product_category import ProductCategory
 from odoo.addons.product.models.product_template import ProductTemplate
 
-from enum import Enum
 
 class SubscriptionMode(str, Enum):
     member = "member"
@@ -15,23 +15,27 @@ class SubscriptionMode(str, Enum):
     company_invited = "company_invited"
     voluntary = "voluntary"
 
+
 class MembershipMode(str, Enum):
     cooperator = "member"
     invited = "invited"
     voluntary = "voluntary"
 
-class MembershipMode(str, Enum):
+
+class MemberTypeMode(str, Enum):
     individual = "individual"
     company = "company"
 
+
 class FormTypeMode(str, Enum):
-    global = "global"
+    base = "global"  # TODO: If global is reserved, change to base
     single = "single"
+
 
 class WebsiteShareSubscriptionContext(BaseModel):
     membership_mode: MembershipMode
     membertype_mode: MemberTypeMode
     formtype_mode: FormTypeMode
-    company: ResCompany
+    company: Company
     product_categ: ProductCategory
     product: Optional[ProductTemplate] = None
