2fa8a6ad7e528211c722bee0e07ca37b88efaee7
Álvaro García <alvarogruiz8@gmail.com>
2025-12-10 10:31:03 +0100

    [FIX] 🐛 Fix api depends on urls field of product form view
    Author: Alvaro Garcia <alvaro.garcia@somit.coop>

--- energy_communities_cooperator/models/product.py
+++ energy_communities_cooperator/models/product.py
@@ -22,7 +22,7 @@ class ProductTemplate(models.Model):
             ).id: "voluntary",
         }
 
-    @api.depends("company_id")
+    @api.depends("company_id", "categ_id")
     def _compute_url(self):
         MAPPING__PRODUCT_CATEG_ID__SUBSCRIPTION_MODE = (
             self.get_mapping_product_category_id_subscription_mode()
@@ -45,7 +45,7 @@ class ProductTemplate(models.Model):
                 record.url_individual = None
                 record.url_company = None
 
-    @api.depends("company_id", "product_external_id")
+    @api.depends("company_id", "product_external_id", "categ_id")
     def _compute_url_specific(self):
         MAPPING__PRODUCT_CATEG_ID__SUBSCRIPTION_MODE = (
             self.get_mapping_product_category_id_subscription_mode()
