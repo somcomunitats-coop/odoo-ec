7ba3912c831b515fb4d13c3e7df6ebb63f87f75f
Álvaro García <alvarogruiz8@gmail.com>
2025-12-22 10:37:33 +0100

    [IMP] ✨ Now you can import 2 lines equals on the same file
    Author: Alvaro Garcia <alvaro.garcia@somit.coop>

--- energy_account_statement_import_file/wizard/account_statement_import.py
+++ energy_account_statement_import_file/wizard/account_statement_import.py
@@ -12,11 +12,17 @@ class AccountStatementImport(models.TransientModel):
         This is used to avoid duplicates when importing statements.
         """
         stmts_vals = super()._complete_stmts_vals(stmts_vals, journal, account_number)
+        unique_import_ids = []
         for st_vals in stmts_vals:
             for lvals in st_vals["transactions"]:
                 if not lvals.get("unique_import_id", False):
                     # Create MD5 hash from all values in st_vals
                     lvals_str = str(sorted(lvals.items()))
-                    lvals_hash = hashlib.md5(lvals_str.encode('utf-8')).hexdigest()
+                    lvals_hash = hashlib.md5(lvals_str.encode("utf-8")).hexdigest()
+                    if lvals_hash in unique_import_ids:
+                        lvals_hash = hashlib.md5(
+                            (lvals_str + str(len(unique_import_ids))).encode("utf-8")
+                        ).hexdigest()
                     lvals["unique_import_id"] = lvals_hash
+                    unique_import_ids.append(lvals_hash)
         return stmts_vals
