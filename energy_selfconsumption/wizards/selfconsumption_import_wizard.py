f96f9b6a3912747deb1feba1006f5976537d3d2c
Álvaro García <alvarogruiz8@gmail.com>
2026-01-29 13:27:28 +0100

    [IMP] ✨ Implement text creation new inscriptions of type invited

--- energy_selfconsumption/wizards/selfconsumption_import_wizard.py
+++ energy_selfconsumption/wizards/selfconsumption_import_wizard.py
@@ -10,6 +10,17 @@ from odoo import _, api, fields, models
 from odoo.exceptions import UserError, ValidationError
 
 from odoo.addons.energy_communities.config import DISPLAY_DATE_FORMAT
+from odoo.addons.energy_communities.utils import product_utils
+from odoo.addons.energy_communities_cooperator.schemas import (
+    MemberTypeMode,
+    SubscriptionRequestCreationParams,
+)
+from odoo.addons.energy_communities_cooperator.utils import (
+    subscription_request_utils,
+)
+from odoo.addons.energy_communities_service_invoicing.schemas import (
+    ServiceProductCreationData,
+)
 
 from ..config import (
     CSV_DELIMITER,
@@ -667,6 +678,36 @@ class SelfconsumptionImportWizard(models.TransientModel):
             participations: Available participation records
             project_id (int): Project ID
         """
+        invited_category = self.env.ref(
+            "energy_communities_cooperator.product_category_company_invited_share"
+        )
+
+        def _invited_product_creation_params(company):
+            return ServiceProductCreationData(
+                company_id=company.id,
+                categ_id=invited_category.id,
+                name="Registro persona/entidad invitada",
+                description_sale=None,
+                default_code="RI",
+                list_price=0,
+                taxes_id=[],
+                short_name="Registro/e invitada/convidada",
+                sale_ok=False,
+                display_on_website=True,
+                default_share_product=True,
+            )
+
+        def _invited_product_translations(invited_product):
+            invited_product.with_context(lang="ca_ES").write(
+                {"name": "Registre persona/entitat convidada"}
+            )
+            invited_product.with_context(lang="es_ES").write(
+                {"name": "Registro persona/entidad invitada"}
+            )
+            invited_product.with_context(lang="eu_ES").write(
+                {"name": "Parte hartzen duen pertsonaren/erakundearen erregistroa"}
+            )
+
         # Generate test data
         vat = self.generate_spanish_vat()
         contracted_power = round(random.uniform(1, 100), 2)
@@ -693,6 +734,82 @@ class SelfconsumptionImportWizard(models.TransientModel):
                 }
             )
         )
+        # Get invited product
+        invited_product = self.env["product.template"].search(
+            [
+                ("categ_id", "=", invited_category.id),
+                ("company_id", "=", self.env.company.id),
+            ],
+            limit=1,
+        )
+        if not invited_product:
+            with product_utils(env, use_sudo=True) as product_component:
+                # create company pricelist
+                product_component.create_company_pricelist(self.env.company)
+                product_component.setup_company_product_categs(self.env.company)
+                # invited product
+                invited_product = product_component.create_product(
+                    _invited_product_creation_params(self.env.company)
+                )
+                _invited_product_translations(invited_product)
+
+        # Create subscription request params
+        try:
+            creation_params = partner.read(
+                [
+                    "email",
+                    "firstname",
+                    "lastname",
+                    "gender",
+                    "birthdate",
+                    "phone",
+                    "lang",
+                    "vat",
+                    "city",
+                    "country_id",
+                    "birthdate",
+                    "company_id",
+                ]
+            )
+            creation_params.update(
+                {
+                    "address": partner.street,
+                    "zip_code": partner.zip,
+                    "ordered_parts": 1,
+                    "iban": "ES2030043137684971748683",
+                    "conditions_payment": True,
+                    "share_product_id": invited_product,
+                    "product_categ": invited_category,
+                    "membertype_mode": MemberTypeMode.individual,
+                    "lang": partner.lang.iso_code,
+                }
+            )
+            subscription_request_creation_params = SubscriptionRequestCreationParams(
+                **creation_params
+            )
+        except Exception as e:
+            msg = "Error creating subscription request params with <Company={}, Partner={}>: {}"
+            logger.error(
+                "Company: %s, Partner: %s", self.env.company.name, partner.name
+            )
+            raise ValueError(msg.format(self.env.company.name, partner.name, str(e)))
+        # Create subscription request
+        # Validate subscription request
+        try:
+            # Use component to validate and create subscription request
+            with subscription_request_utils(self.env) as component:
+                subscription_request = component.create_subscription_request(
+                    subscription_request_creation_params
+                )
+                subscription_request.validate_subscription_request()
+        except Exception as e:
+            msg = (
+                "Error creating subscription request with <Company={}, Partner={}>: {}"
+            )
+            logger.error(
+                "Company: %s, Partner: %s", self.env.company.name, partner.name
+            )
+            raise ValueError(msg.format(self.env.company.name, partner.name, str(e)))
 
         # Create supply point
         supply_point = (
